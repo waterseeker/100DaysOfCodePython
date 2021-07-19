c58be292874cdfdf2433248a9d9d95455204bb1d
Wayne Burris <waterseeker@Hotmail.com>
2021-07-19 08:07:28 -0500

    first exercise

---
+++ Day1/main.py
@@ -0,0 +1,8 @@
+# Print the following over 3 lines:
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
