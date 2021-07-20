680a9abc823770b0d96bbafc11368357297b8843
Wayne Burris <waterseeker@Hotmail.com>
2021-07-20 14:40:58 -0500

    changed some variable names to be mroe readable

--- Day2/day-2-2.py
+++ Day2/day-2-2.py
@@ -18,7 +18,7 @@ weight = input("enter your weight in kg: ")
 # Write your code below this line 👇
 # This exercise is assuming the user is passing appropriate data for inputs
 # BMI should be converted to a whole number
-height_cast_to_float = float(height)
-weight_cast_to_int = int(weight)
-user_bmi = int(weight_cast_to_int / height_cast_to_float ** 2)
+height_as_float = float(height)
+weight_as_int = int(weight)
+user_bmi = int(weight_as_int / height_as_float ** 2)
 print("Your bmi is : " + str(user_bmi))
