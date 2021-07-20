90f36dfb7549283afcb611dc9994c9e81e1e0b40
Wayne Burris <waterseeker@Hotmail.com>
2021-07-20 15:40:05 -0500

    added formatting to total output instead of if else block

--- Day2/day-2-4.py
+++ Day2/day-2-4.py
@@ -4,7 +4,8 @@
 #Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 print("Welcome to the tip calculator.")
 total_bill = input("What was the total bill? $")
-tip_percentage = input("What percentage tip would you like to give? ")
+# This is assuming the user just inputs a number, no %
+tip_percentage = input("What percentage tip would you like to give?")
 number_of_diners = input("How many people to split the bill? ")
 
 total_as_float = float(total_bill)
@@ -14,9 +15,6 @@ tip_as_decimal = tip_percentage_as_float / 100
 tip = total_as_float * tip_as_decimal
 total_bill_with_tip = total_as_float + tip
 total_per_diner = round(total_bill_with_tip / number_of_diners_as_int, 2)
-# round can return only 1 decimal place if the float parameter only has 1 decimal space
-# i.e. 9.0, so you can add a 0 in this case to return the correctly formatted answer.
-if len(str(total_per_diner).rsplit('.')[-1]) != 2:
-    print(f"Each person should pay: ${total_per_diner}0")
-else:
-    print(f"Each person should pay: ${total_per_diner}")
+formatted_total_per_diner = "{:.2f}".format(total_per_diner)
+print(f"Each person should pay: ${formatted_total_per_diner}")
+    
