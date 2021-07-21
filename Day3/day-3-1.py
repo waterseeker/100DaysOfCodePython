6504534083d6a11458319086634759e76c53ef22
Wayne Burris <waterseeker@Hotmail.com>
2021-07-21 16:24:49 -0500

    complete day-3-1.py

--- Day3/day-3-1.py
+++ Day3/day-3-1.py
@@ -70,3 +70,7 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
