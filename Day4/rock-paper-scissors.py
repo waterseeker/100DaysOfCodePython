bb187a1d7bd4bd9344979865294fbf298a3ba934
Wayne Burris <waterseeker@Hotmail.com>
2021-07-22 15:47:25 -0500

    start of rock paper scissors

---
+++ Day4/rock-paper-scissors.py
@@ -0,0 +1,31 @@
+# Make a rock, paper, scissors game where the user inputs 0, 1, or 2 for their choice.
+# The game should display the asci art for the players choice and the computers choice.
+# There should be a message telling the player whether they win, lose, or tie.
+# In the case of a tie, ask the player to choose again until there is a winner.
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
