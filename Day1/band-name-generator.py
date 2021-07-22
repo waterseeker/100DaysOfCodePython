abbc7d4f347443787909b9c832ff59e3fbb2883a
Wayne Burris <waterseeker@Hotmail.com>
2021-07-22 14:51:50 -0500

    changed filenames to be more descriptive, start of heads or tails exercise

--- Day1/band-name-generator.py
+++ Day1/band-name-generator.py
@@ -5,6 +5,6 @@ user_city = input("What city did you grow up in?\n")
 #3. Ask the user for the name of a pet.
 user_pet = input("What was the name of your pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
-print("Your band name is " + user_city + " " + user_pet)
+print("Your band name is " + user_city + " " + user_pet + ".")
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
