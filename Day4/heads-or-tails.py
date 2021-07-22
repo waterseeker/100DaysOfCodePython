b75fe35fbf8f828b589f634cf9be8e95a1e79101
Wayne Burris <waterseeker@Hotmail.com>
2021-07-22 14:52:39 -0500

    fixed instructions for heads or tails

--- Day4/heads-or-tails.py
+++ Day4/heads-or-tails.py
@@ -2,22 +2,22 @@
 
 # Instructions
 
-You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails". 
+# You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails". 
 
-**Important**, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+# **Important**, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
 
-There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
+# There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
 
-e.g.
-1 means Heads
-0 means Tails 
+# e.g.
+# 1 means Heads
+# 0 means Tails 
 
 # Example Output
-Heads
+# Heads
 
-or
+# or
 
-Tails
+# Tails
 
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
