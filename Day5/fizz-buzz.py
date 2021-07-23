26479b9939ae97beea8c0e8aefc419fea720ca7e
Wayne Burris <waterseeker@Hotmail.com>
2021-07-23 15:37:25 -0500

    finished first pass of fizz buzz

--- Day5/fizz-buzz.py
+++ Day5/fizz-buzz.py
@@ -40,3 +40,12 @@
 # 2. Each number/text should be printed on a separate line.
 
 #Write your code below this row 👇
+for number in range(1, 101):
+    if number % 15 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
