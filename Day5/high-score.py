f364d407bb8cb1d887dc3e355f1a895810e01938
Wayne Burris <waterseeker@Hotmail.com>
2021-07-23 15:16:25 -0500

    start of high score calculator

---
+++ Day5/high-score.py
@@ -0,0 +1,28 @@
+## Highest Score
+
+# Instructions
+
+# You are going to write a program that calculates the highest score from a List of scores. 
+
+# e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+# **Important** you are not allowed to use the max or min functions. The output words must match the example. i.e 
+
+# > The highest score in the class is: x
+
+# Example Input 
+# 78 65 89 86 55 91 64 89
+
+# In this case, student_scores would be a list that looks like: `[78, 65, 89, 86, 55, 91, 64, 89]`
+
+# Example Output 
+# The highest score in the class is: 91
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
