040846936b2c2fbdfde259588e61437025babc53
Wayne Burris <waterseeker@Hotmail.com>
2021-07-24 16:44:55 -0500

    added more instruction

--- Day6/reeborg's_world_hurdles.py
+++ Day6/reeborg's_world_hurdles.py
@@ -1,5 +1,7 @@
 # this challenge is to solve:
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# this is a game that already has some built-in functions and you use those to make more complex functions 
+# to complete the challenges
 def turn_right():
     turn_left()
     turn_left()
