06718df5e280b352969b96e5d9f5dde9b501bad8
Wayne Burris <waterseeker@Hotmail.com>
2021-07-26 17:01:05 -0500

    working on hangman step 4, problem with lives count

--- Day7/hangman_step_4.py
+++ Day7/hangman_step_4.py
@@ -67,6 +67,7 @@ word_length = len(chosen_word)
 #TODO-1: - Create a variable called 'lives' to keep track of the number of 
 # lives left. 
 #Set 'lives' to equal 6.
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -89,7 +90,11 @@ while not end_of_game:
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print 
     # "You lose."
-
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose.")
+            end_of_game == True
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -97,6 +102,9 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the 
     # current number of 'lives' the user has remaining.
+    print(stages[lives])
+    if lives == 0:
+        end_of_game == True
+    print(f'lives: {lives}')
