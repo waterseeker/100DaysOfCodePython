1e29c88ed88550b87454334895479c8f3e39fc16
Wayne Burris <waterseeker@Hotmail.com>
2021-08-04 20:40:01 -0500

    finished first pass of secret auction

---
+++ Day9/secret_auction.py
@@ -0,0 +1,33 @@
+# Print the logo
+# The program asks for a user name and a bid on an item.
+# The program asks if there is another bidder after each person is done.
+# Store names and bids in a dictionary.
+# Print out the winning bidder and bid after everyone has bid.
+from art import logo
+from os import system
+
+def clear():
+    _ = system('clear')
+
+def secret_auction():
+    print(logo)
+    print('Welcome to the secret auction program.')
+    bidding = True
+    bids = {}
+    while True:
+        user_name = input('What is your name?: ')
+        bid = input("What's your bid?: $")
+        bids[user_name] = bid
+        next_user = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+        if next_user == 'no':
+            bid_values = bids.values()
+            highest_bid = max(bid_values)
+            index_of_highest_bid = list(bids.values()).index(highest_bid)
+            highest_bidder = list(bids.keys())[index_of_highest_bid]
+            print(f'The winner is {highest_bidder} with a bid of ${highest_bid}.')
+            break
+        elif next_user == 'yes':
+            clear()
+            continue    
+
+secret_auction()        
