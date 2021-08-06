414928546d69a74645cfc36803efbfc11e5cdcc9
Wayne Burris <waterseeker@Hotmail.com>
2021-08-06 18:50:52 -0500

    changed output to bools

--- Day10/days_in_month.py
+++ Day10/days_in_month.py
@@ -36,13 +36,13 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+      return True
   else:
-    print("Not leap year.")
+    return False
 
 def days_in_month():
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
