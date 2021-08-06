9dfd882a7cba1f00f569c61290132c3730b2bf90
Wayne Burris <waterseeker@Hotmail.com>
2021-08-05 20:28:28 -0500

    code cleanup, added some notation

--- Day9/secret_auction.py
+++ Day9/secret_auction.py
@@ -6,19 +6,19 @@
 from art import logo
 from os import system
 
+# function to clear the console when there is a new bidder
 def clear():
     _ = system('clear')
 
 def secret_auction():
     print(logo)
     print('Welcome to the secret auction program.')
-    bidding = True
     bids = {}
     while True:
         user_name = input('What is your name?: ')
         bid = input("What's your bid?: $")
         bids[user_name] = bid
-        next_user = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+        next_user = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
         if next_user == 'no':
             bid_values = bids.values()
             highest_bid = max(bid_values)
