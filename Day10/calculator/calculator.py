62b3af673f7c1001b401d785d89ebcf01539fdaa
Wayne Burris <waterseeker@Hotmail.com>
2021-08-07 14:50:17 -0500

    first pass of calculator

---
+++ Day10/calculator/calculator.py
@@ -0,0 +1,39 @@
+#Calculator
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    return n1 * n2
+    
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+    
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide,
+}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
+num2 = int(input("What's the second number?: "))
+
+math_function = operations[operation_symbol]
+
+answer = math_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
