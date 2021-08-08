1788e20e2465c9dedd04f4f4fae72189e09f459a
Wayne Burris <waterseeker@Hotmail.com>
2021-08-08 14:12:47 -0500

    added lopping with recursion

--- Day10/calculator/calculator.py
+++ Day10/calculator/calculator.py
@@ -1,4 +1,5 @@
 #Calculator
+from art import logo
 
 #Add
 def add(n1, n2):
@@ -23,17 +24,27 @@ operations = {
     '/': divide,
 }
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
 
-for symbol in operations:
-    print(symbol)
+    for symbol in operations:
+        print(symbol)
 
-operation_symbol = input("Pick an operation from the line above: ")
+    should_continue = True
 
-num2 = int(input("What's the second number?: "))
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        math_function = operations[operation_symbol]
+        answer = math_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-math_function = operations[operation_symbol]
+        if input(f'Type "y" to continue calculating with {answer} or type "n" to start a new calculation.: ') == 'y':
+            num1 = answer
+            continue
+        else:
+            should_continue = False
+            calculator()
 
-answer = math_function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
