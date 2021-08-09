410acb1896e12ed77122784f30547a42190ae244
Wayne Burris <waterseeker@Hotmail.com>
2021-08-09 18:18:32 -0500

    working on program flow

--- Day11/test_black_jack.py
+++ Day11/test_black_jack.py
@@ -23,27 +23,30 @@ from art import logo
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# hand = random.choices(cards, k=2)
+hand = [11, 10]
 
 def get_score(hand_array):
+    print(hand_array)
     if 11 not in hand_array:
         score = sum(hand_array)
     score = sum(hand_array)
     if score == 21:
         print(f'Your score is : {score}')
+get_score(hand)
+# start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+# if start_game == 'y':
+#     play_game = True
+#     print(logo)
 
-start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-if start_game == 'y':
-    play_game = True
-    print(logo)
-
-while play_game:
-    player_hand = [random.choice(cards), random.choice(cards)]
-    player_score = get_score(player_hand)
-    dealer_hand = [random.choice(cards), random.choice(cards)]
+# while play_game:
+#     player_hand = [random.choice(cards), random.choice(cards)]
+#     player_score = get_score(player_hand)
+#     dealer_hand = [random.choice(cards), random.choice(cards)]
     
 
     
-    print(f'Your cards: {player_hand}')
-    print(f'Computer"s first card: {dealer_hand[0]}')
+#     print(f'Your cards: {player_hand}')
+#     print(f'Computer"s first card: {dealer_hand[0]}')
 
-    play_game = False
+#     play_game = False
