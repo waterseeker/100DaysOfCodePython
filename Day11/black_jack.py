63f95f3bcc3a73806034a1d3deb257a85e377547
Wayne Burris <waterseeker@Hotmail.com>
2021-08-11 18:18:06 -0500

    moved some repeated logic into functions, working on win and loss conditions checking logic

--- Day11/black_jack.py
+++ Day11/black_jack.py
@@ -24,7 +24,6 @@ import random
 from collections import Counter
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# may need flags for player has blackjack and computer has blackjack
 
 def get_score(hand_array):
     # is it blackjack?
@@ -33,6 +32,7 @@ def get_score(hand_array):
             return 'blackjack'
     number_of_aces = Counter(hand_array)[11]
     non_ace_total = sum(card for card in hand_array if card != 11)
+    ace_value = 0
     if number_of_aces > 0:
         if non_ace_total + 11 + (number_of_aces - 1) <= 21:
             ace_value = 11 + (number_of_aces - 1)
@@ -42,7 +42,28 @@ def get_score(hand_array):
     return hand_total
 
 def draw_card(hand_array):
-    hand_array += random.choice(cards)
+    hand_array.append(random.choice(cards))
+
+def dealer_draw(dealer_hand):
+    if dealer_score >= 17:
+        print("The dealer stands.")
+        print(f"Dealer's hand: {dealer_hand}")
+        return
+    if dealer_score < 17:
+        print("The dealer draws another card.")
+        draw_card(dealer_hand)
+        print(f"Dealer's hand: {dealer_hand}")
+
+def print_hands():
+    print(f'Your cards: {player_hand}')
+    print(f"Dealer's cards: {dealer_hand}")
+
+def start_again():
+    start_again = input("Do you want to play again? 'y' or 'n': ")
+    if start_again == 'y':
+        return True
+    else:
+        return False
 
 start_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 if start_game == 'y':
@@ -57,31 +78,31 @@ while play_game:
     dealer_hand = [random.choice(cards), random.choice(cards)]
     dealer_score = get_score(dealer_hand)
     if player_score == 'blackjack' and dealer_score != 'blackjack':
-        print(f'Your cards: {player_hand}')
-        print(f"Dealer's cards: {dealer_hand}")
+        print_hands()
         print('Blackjack! You win!')
-        start_again = input("Do you want to play again? 'y' or 'n': ")
+        start_again()
         if start_again:
             continue
         else:
+            play_game = False
             break
     elif player_score != 'blackjack' and dealer_score == 'blackjack':
-        print(f'Your cards: {player_hand}')
-        print(f"Dealer's cards: {dealer_hand}")
+        print_hands()
         print('The dealer wins with Blackjack!')
-        start_again = input("Do you want to play again? 'y' or 'n': ")
+        start_again()
         if start_again:
             continue
         else:
+            play_game = False
             break
     elif player_score == 'blackjack' and dealer_score == 'blackjack':
-        print(f'Your cards: {player_hand}')
-        print(f"Dealer's cards: {dealer_hand}")
+        print_hands()
         print("It's a tie! You both have Blackjack. What are the odds?")
-        start_again = input("Do you want to play again? 'y' or 'n': ")
+        start_again()
         if start_again:
             continue
         else:
+            play_game = False
             break
     else:
         print(f'Your cards: {player_hand}')
@@ -93,21 +114,15 @@ while play_game:
             player_score = get_score(player_hand)
             if player_score > 21:
                 print("You bust!")
-                start_again = input("Do you want to play again? 'y' or 'n': ")
+                start_again()
                 if start_again:
                     continue
                 else:
+                    play_game = False
                     break
             if player_score == 21:
                 print("You have 21. It doesn't get better than that so you stand.")
-            if dealer_score >= 17:
-                print("The dealer stands.")
-                print(f"Dealer's first card: {dealer_hand[0]}")
-            if dealer_score < 17:
-                print("The dealer draws another card.")
-                draw_card(dealer_hand)
-                print(f'Computer"s first card: {dealer_hand[0]}')
-        else:
+        dealer_draw(dealer_hand)
 
 
 
