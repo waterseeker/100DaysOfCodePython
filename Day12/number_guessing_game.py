7ea6361184d72fd5584bbd383cb9a764a11f890c
Wayne Burris <waterseeker@Hotmail.com>
2021-08-16 19:46:52 -0500

    start of number guessing game

---
+++ Day12/number_guessing_game.py
@@ -0,0 +1,12 @@
+print('Welcome to the Number Guessing Game!')
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+# player gets a number of guesses based on difficulty 
+#  easy =      hard = 5
+# player guesses
+# gets message telling them whether their guess is too high, too low, or correct.
+# if they didn't guess it, they get a message
+# print("Guess again.")
+# print(f"You have {number_of_guesses} attempts remaning to guess the number.")
+player_guess = input("Make a guess: ")
