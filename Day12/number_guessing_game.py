8b4ee32999577ff647f2f58bfda650b7af276265
Wayne Burris <waterseeker@Hotmail.com>
2021-08-16 20:08:26 -0500

    finished first pass of number guessing game

--- Day12/number_guessing_game.py
+++ Day12/number_guessing_game.py
@@ -1,30 +1,45 @@
 import random
 
 print('Welcome to the Number Guessing Game!')
-print("I'm thinking of a number between 1 and 100.")
+print("I'm thinking of a whole number between 1 and 100.")
 answer = random.randint(0,100)
-
+playing_game = True
 while True:
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
     if difficulty == 'easy':
         player_guesses = 10
+        guesses_remaining = player_guesses
         break
     elif difficulty == 'hard':
         player_guesses = 5
+        guesses_remaining = player_guesses
         break
     else:
         print("What's that? Sorry you have to pick either 'easy' or 'hard'.")
-# player gets a number of guesses based on difficulty 
-#  easy = 10     hard = 5
+
 # player guesses
-# gets message telling them whether their guess is too high, too low, or correct.
-# if they didn't guess it, they get a message
-# print("Guess again.")
-# print(f"You have {number_of_guesses} attempts remaning to guess the number.")
-player_guess = input("Make a guess: ")
-
-# if the player runs out of guesses:
-# print("You've run our of guesses. You lose.")
-
-# if the player guesses the number:
-# print(f"You got it! The answer was {answer}")
+while playing_game == True:
+    if guesses_remaining == 0:
+        print("You've run our of guesses. You lose.")
+        playing_game = False
+        break
+    player_guess = input("Make a guess: ")
+    if int(player_guess):
+        player_guess = int(player_guess)
+        if player_guess > answer:
+            guesses_remaining -= 1
+            print('Too high.')
+            print('Guess again.')
+            print(f"You have {guesses_remaining} attempts remaning to guess the number.")
+            continue
+        elif player_guess < answer:
+            guesses_remaining -= 1
+            print('Too low.')
+            print('Guess again.')
+            print(f"You have {guesses_remaining} attempts remaning to guess the number.")
+            continue
+        else:
+            print(f"You got it! The answer was {answer}.")
+            break
+    else:
+        print("Sorry, you have to guess a whole number.")
