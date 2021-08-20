b53d3b8a5904ae92c9e68450eea7edff41dd72c5
Wayne Burris <waterseeker@Hotmail.com>
2021-08-20 11:47:29 -0500

    working on game logic

--- Day14/higher_lower.py
+++ Day14/higher_lower.py
@@ -7,11 +7,20 @@ data_length = len(data)
 def getData(data):
     return choice(data)
 
-first_person = getData(data)
-second_person = getData(data)
-print(first_person)
+entry_A = getData(data)
+entry_B = getData(data)
+print(entry_A)
 print(logo)
-print(f'Compare A: {first_person}, a {first_person}, from {first_person}.')
+print(f'Compare A: {entry_A}, a {entry_A}, from {entry_A}.')
 print(vs)
-print(f'Against B: {second_person}, a {second_person}, from {second_person}.')
+print(f'Against B: {entry_B}, a {entry_B}, from {entry_B}.')
 user_choice = input("Who has more followers? Type 'A' or 'B': ")
+
+# if the player guesses correctly,
+score += 1
+print(f"You're right! Current score: {score}")
+#   the correct answer becomes entry A for the next round
+#   get a new entry from data that becomes entry B for the next round
+
+# game ends when the player get an incorrect answer
+print(f"Sorry, that's wrong. Final score: {score}")
