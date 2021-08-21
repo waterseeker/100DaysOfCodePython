193761b416bbb4e272bc0ec0fbc3c643a443b69f
Wayne Burris <waterseeker@Hotmail.com>
2021-08-21 11:01:33 -0500

    adding todos

--- Day15/coffee_machine.py
+++ Day15/coffee_machine.py
@@ -31,9 +31,30 @@ resources = {
 }
 
 coffee_emoji = '☕'
-# // TODO 1 - Prompt user by asking 
+
+
+# // TODO 1 - Prompt user by asking
 #   "What would you like? (espresso/latte/cappuccino)"
-# // TODO - print report of all coffee machine resources
-# When the user enters “report” to the prompt, a report should be generated
-# that showsthe current resource values.
-# e.g.Water: 100mlMilk: 50mlCoffee: 76gMoney: $2.5
+#   a. Check the user's input to decide what to do next.
+#   b. The prompt should show every time action has completed, e.g. once the
+#       drink is dispensed. The prompt should show again to serve the next
+#       customer.
+# // TODO 2 - Turn off the Coffee Machine by entering "off" to the prompt.
+#   a. For maintainers of the coffee machine, they can use "off" as the secret
+#       word to turn off the machine. Your code should end execution when this
+#       happens.
+# // TODO 3 - Print report.
+#   a. When the user enters "report" to the prompt, a report should be
+#       generated that shows the current resource values e.g.
+#       Water: 100ml
+#       Milk: 50ml
+#       Coffee: 76g
+#       Money: $2.5
+# // TODO 4 - Check resources sufficient?
+#   a. When the user chooses a drink, the program should check if there are
+#       enough resources to make that drink.
+#   b. E.g. if Latte requires 200ml water but there is only 100ml left
+#       in the machine, it should not continue to make the drink but
+#       print "Sorry there is not enough water."
+#   c. The same should happen if another resource is depleted,
+#       e.g. milk or coffee.
