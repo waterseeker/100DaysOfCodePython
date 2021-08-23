a2f4a48242d5de4ad740e8780df81421ec8ff2a0
Wayne Burris <waterseeker@Hotmail.com>
2021-08-23 05:41:30 -0500

    finished first pass of coffee machine

--- Day15/coffee_machine.py
+++ Day15/coffee_machine.py
@@ -106,64 +106,24 @@ while play_game:
                                      number_of_dimes * 0.10 +
                                      number_of_nickels * 0.05 +
                                      number_of_pennies * 0.01)
-                resources["water"] -= (MENU[user_input]
-                                           ["ingredients"]
-                                           ["water"])
-                resources["coffee"] -= (MENU[user_input]
-                                            ["ingredients"]
-                                            ["coffee"])
-                if "milk" in MENU[user_input]["ingredients"]:
-                    resources["milk"] -= (MENU[user_input]
-                                              ["ingredients"]
-                                              ["milk"])
+                if total_coins_input < drink_price:
+                    print("Sorry that's not enough money. Money refunded.")
+                    break
+                elif total_coins_input >= drink_price:
+                    resources["water"] -= (MENU[user_input]
+                                               ["ingredients"]
+                                               ["water"])
+                    resources["coffee"] -= (MENU[user_input]
+                                                ["ingredients"]
+                                                ["coffee"])
+                    if "milk" in MENU[user_input]["ingredients"]:
+                        resources["milk"] -= (MENU[user_input]
+                                                  ["ingredients"]
+                                                  ["milk"])
+                    if total_coins_input > drink_price:
+                        change = round(total_coins_input - drink_price, 2)
+                        print(f"Here is {change} dollars in change.")
+                        resources["money"] += drink_price
+                    drink_name = user_input
+                    print(f"Here is your {drink_name} {coffee_emoji}. Enjoy!")
             continue
-#   a. Check the user's input to decide what to do next.
-#   b. The prompt should show every time action has completed, e.g. once the
-#       drink is dispensed. The prompt should show again to serve the next
-#       customer.
-# // TODO 5 - Process coins.
-#   a. If there are sufficient resources to make the drink selected,
-#       then the program should prompt the user to insert coins.
-#   b. Remember tat quarters = $0.25, dimes = $0.10, nickels = $0.05,
-#       and pennies = $0.01
-#   c. Calculate the monetary value of the coins inserted. E.g. 1 quarter,
-#       2 dimes, 1 nickel, 2 pennies = 0.25 + 2 * .10 + .05 + .01 * 2 = $0.52
-#   d. Prompt the user: (each of these on a new line)
-#       "how many quarters?: "
-#       "how many dimes?: "
-#       "how many nickles?: "
-#       "how many pennies?: "
-# // TODO 6 - Check transaction successful?
-#   a. Check that the user has inserted enough money to purchase the drink
-#       they selected. E.g. Latte cost $2.50, but they only inserted $0.52
-#       then the program should say "Sorry that's not enough money. Money
-#       refunded."
-#   b. But if the user has inserted enough money, then the cost of the drink
-#       gets added to the machine as the profit and this will be reflected
-#       the next time "report" is triggered. E.g.
-#       Water: 100ml
-#       Milk: 50ml
-#       Coffee: 76g
-#       Money: $2.5
-#   c. If the user has inserted too much money, the machine should offer
-#       change. E.g. "Here is $2.45 dollars in change." The change should be
-#       rounded to the nearest 2 decimal places.
-# // TODO 7 - Make Coffee.
-#   a. If the transaction is successful and there are enough resources to make
-#       the drink the user selected, then the ingredients to make the drink
-#       should be deducted from the coffee machine resources.
-#
-#       E.g. report before purchasing latte:
-#       Water: 300ml
-#       Milk: 200ml
-#       Coffee: 100g
-#       Money: $0
-#
-#       report after purchasing latte:
-#       Water: 100ml
-#       Milk: 50ml
-#       Coffee: 76g
-#       Money: $2.5
-#   b. Once all resources have been deducted, tell the user
-#       "Here is your latte ☕️. Enjoy!".
-#       If latte was their choice of drink.
