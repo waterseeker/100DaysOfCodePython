6463de1a00f73477e6ec16e2d05cea6e63f1061a
Wayne Burris <waterseeker@Hotmail.com>
2021-08-24 14:28:24 -0500

    first pass of turtle

--- Day16/mock_turtle.py
+++ Day16/mock_turtle.py
@@ -2,3 +2,10 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canyheight)
+my_screen.exitonclick()
