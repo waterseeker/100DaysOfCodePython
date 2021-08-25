b414385533a12e2dd29c05ccaf735c38dfb0e56c
Wayne Burris <waterseeker@Hotmail.com>
2021-08-25 14:39:20 -0500

    changed alignment

--- Day16/example_pretty_table.py
+++ Day16/example_pretty_table.py
@@ -11,5 +11,6 @@ table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 
-table.align = "r"
+table.align = "l"
+
 print(table)
