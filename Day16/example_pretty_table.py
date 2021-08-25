87e87554f820acaed9534ff87113eb8c39a5435a
Wayne Burris <waterseeker@Hotmail.com>
2021-08-25 14:34:15 -0500

    added alternate way to populate the table data

--- Day16/example_pretty_table.py
+++ Day16/example_pretty_table.py
@@ -2,9 +2,13 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 
-table.field_names = ["Pokeomon Name", "Type"]
-table.add_row(["Pikachu", "Electric"])
-table.add_row(["Squirtle", "Water"])
-table.add_row(["Charmander", "Fire"])
+# table.field_names = ["Pokeomon Name", "Type"]
+# table.add_row(["Pikachu", "Electric"])
+# table.add_row(["Squirtle", "Water"])
+# table.add_row(["Charmander", "Fire"])
+
+# Another way to do the same thing
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
 print(table)
