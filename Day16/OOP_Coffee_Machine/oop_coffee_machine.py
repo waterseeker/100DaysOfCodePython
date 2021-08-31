9ca238d8dd318690674bc1780d4330fd13f6cddc
Wayne Burris <waterseeker@Hotmail.com>
2021-08-31 15:29:25 -0500

    finished first pass of oop coffee machine

--- Day16/OOP_Coffee_Machine/oop_coffee_machine.py
+++ Day16/OOP_Coffee_Machine/oop_coffee_machine.py
@@ -12,7 +12,6 @@ while play_game:
     while True:
         user_input = input(
             " What would you like? (espresso/latte/cappuccino): ").lower()
-        drink = drink_menu.find_drink(user_input)
         if user_input == 'off':
             play_game = False
             break
@@ -22,34 +21,10 @@ while play_game:
             print("Sorry, that's not a valid choice. Try again.")
             continue
         else:
+            drink = drink_menu.find_drink(user_input)
             enough_resources = coffee_machine.is_resource_sufficient(drink)
             if enough_resources is True:
-                # TODO use money_machine here
-                total_coins_input = cashier.process_coins()
                 drink_price = drink.cost
                 sufficient_funds = cashier.make_payment(drink_price)
-                # if sufficient_funds:
-                #   use resources
-                #   give change if needed
-                #   print drink delivery message
-                if total_coins_input < drink_price:
-                    print("Sorry that's not enough money. Money refunded.")
-                    break
-                elif total_coins_input >= drink_price:
-                    resources["water"] -= (MENU[user_input]
-                                               ["ingredients"]
-                                               ["water"])
-                    resources["coffee"] -= (MENU[user_input]
-                                                ["ingredients"]
-                                                ["coffee"])
-                    if "milk" in MENU[user_input]["ingredients"]:
-                        resources["milk"] -= (MENU[user_input]
-                                                  ["ingredients"]
-                                                  ["milk"])
-                    if total_coins_input > drink_price:
-                        change = round(total_coins_input - drink_price, 2)
-                        print(f"Here is {change} dollars in change.")
-                        resources["money"] += drink_price
-                    drink_name = user_input
-                    print(f"Here is your {drink_name} {coffee_emoji}. Enjoy!")
+                coffee_machine.make_coffee(drink)
             continue
