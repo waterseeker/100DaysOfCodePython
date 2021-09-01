20992745f2df18cfc81fa037dd128f9d25d63c37
Wayne Burris <waterseeker@Hotmail.com>
2021-09-01 15:27:11 -0500

    removed unusee variable

--- Day16/OOP_Coffee_Machine/oop_coffee_machine.py
+++ Day16/OOP_Coffee_Machine/oop_coffee_machine.py
@@ -23,6 +23,6 @@ while play_game:
             enough_resources = coffee_maker.is_resource_sufficient(drink)
             if enough_resources is True:
                 drink_price = drink.cost
-                sufficient_funds = money_machine.make_payment(drink_price)
+                money_machine.make_payment(drink_price)
                 coffee_maker.make_coffee(drink)
             continue
