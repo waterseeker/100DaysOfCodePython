78ec79280fedc768b3b8ee77a403129aec2ae60f
Wayne Burris <waterseeker@Hotmail.com>
2021-09-04 08:29:17 -0500

    continuation of pep8 cleanup

--- Day14/higher_lower.py
+++ Day14/higher_lower.py
@@ -30,14 +30,17 @@ while play_game:
         entry_B = getData(data)
         continue
     else:
-        print(f'Compare A: {entry_A["name"]}, a {entry_A["description"]}, from {entry_A["country"]}.')
+        print(f'Compare A: {entry_A["name"]},\
+              a {entry_A["description"]}, from {entry_A["country"]}.')
         print(vs)
-        print(f'Against B: {entry_B["name"]}, a {entry_B["description"]}, from {entry_B["country"]}.')
+        print(f'Against B: {entry_B["name"]},\
+              a {entry_B["description"]}, from {entry_B["country"]}.')
         most_followers = max(x['follower_count'] for x in choices)
         answer = ([x for x in choices if x['follower_count']
                   == most_followers][0])
         while True:
-            user_choice = input("Who has more followers? Type 'A' or 'B': ").upper()
+            user_choice = input("Who has more followers? Type 'A' or 'B': ")\
+                .upper()
             if user_choice != 'A' and user_choice != 'B':
                 print('Sorry, you have to choose A or B.')
                 continue
@@ -53,7 +56,7 @@ while play_game:
                     entry_B['choice'] = 'B'
                     choices = [entry_A, entry_B]
                     break
-                else:  
+                else:
                     print(f"Sorry, that's wrong. Final score: {score}")
                     play_game = False
                     break
