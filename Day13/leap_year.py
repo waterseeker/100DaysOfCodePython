16557da0c2d0945383685dcddeaea2a2a51c173c
Wayne Burris <waterseeker@Hotmail.com>
2021-09-06 05:25:21 -0500

    pep8 cleanup

--- Day13/leap_year.py
+++ Day13/leap_year.py
@@ -1,11 +1,12 @@
-## Debug Leap Year
+# Debug Leap Year
 
 # Instructions
 
 # - Read this the code in main.py
-# - Spot the problems 🐞. 
-# - Modify the code to fix the program. 
-# - No shortcuts - don't copy-paste to replace the code entirely with a working solution. 
+# - Spot the problems 🐞.
+# - Modify the code to fix the program.
+# - No shortcuts - don't copy-paste to replace the code entirely with a
+#       working solution.
 
 # Fix the code so that it works.
 
@@ -34,12 +35,12 @@
 year = int(input("Which year do you want to check?"))
 
 if year % 4 == 0:
-  if year % 100 == 0:
-    if year % 400 == 0:
-      print("Leap year.")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
     else:
-      print("Not leap year.")
-  else:
-    print("Leap year.")
+        print("Leap year.")
 else:
-  print("Not leap year.")
+    print("Not leap year.")
