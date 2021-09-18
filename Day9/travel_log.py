559fb6dd7d06de027b12698b4c7a75c628039c08
Wayne Burris <waterseeker@Hotmail.com>
2021-09-18 17:05:14 -0500

    pep8 cleanup

--- Day9/travel_log.py
+++ Day9/travel_log.py
@@ -1,12 +1,12 @@
-## Dictionary in List
+# # Dictionary in List
 
 # Instructions
 
-# You are going to write a program that adds to a travel_log. 
-# You can see a travel_log which is a List that contains 2 Dictionaries. 
+# You are going to write a program that adds to a travel_log.
+# You can see a travel_log which is a List that contains 2 Dictionaries.
 
-# Write a function that will work with the following line of code on line 11 
-# to add the entry for Russia to the travel_log. 
+# Write a function that will work with the following line of code on line 11
+# to add the entry for Russia to the travel_log.
 
 # add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 
@@ -14,43 +14,41 @@
 
 # > `You've been to Moscow and Saint Petersburg.`
 
-# **DO NOT** modify the travel_log directly. 
-# You need to create a function that modifies it. 
+# **DO NOT** modify the travel_log directly.
+# You need to create a function that modifies it.
 
 # Hint
 
-# 1. Look at the function call above to see what the name of the function should be.
+# 1. Look at the function call above to see what the name of the function
+#   should be.
 
-# 2. The inputs for the function are positional arguments. 
+# 2. The inputs for the function are positional arguments.
 #     The order is very important.
 
 # 3. Feel free to choose your own parameter names.
 
 
 travel_log = [
-{
-  "country": "France",
-  "visits": 12,
-  "cities": ["Paris", "Lille", "Dijon"]
-},
-{
-  "country": "Germany",
-  "visits": 5,
-  "cities": ["Berlin", "Hamburg", "Stuttgart"]
-},
+    {
+      "country": "France",
+      "visits": 12,
+      "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+      "country": "Germany",
+      "visits": 5,
+      "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
 ]
-#🚨 Do NOT change the code above
+# 🚨 Do NOT change the code above
 
-#TODO: Write the function that will allow new countries
-#to be added to the travel_log. 👇
+
 def add_new_country(country_name, times_visited, list_of_cities_visited):
-    travel_log.append({'country': country_name, 
-                        'visits': times_visited, 
-                        'cities':list_of_cities_visited})
+    travel_log.append({'country': country_name,
+                       'visits': times_visited,
+                       'cities': list_of_cities_visited})
 
-#🚨 Do not change the code below
+
+# 🚨 Do not change the code below
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
-
-
-
