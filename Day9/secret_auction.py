218ac9fbb4af3c197254eea702ec7645f054a340
Wayne Burris <waterseeker@Hotmail.com>
2021-09-19 06:21:08 -0500

    pep8 cleanup

--- Day9/secret_auction.py
+++ Day9/secret_auction.py
@@ -6,9 +6,11 @@
 from art import logo
 from os import system
 
+
 # function to clear the console when there is a new bidder
 def clear():
     _ = system('clear')
+
 
 def secret_auction():
     print(logo)
@@ -18,16 +20,19 @@ def secret_auction():
         user_name = input('What is your name?: ')
         bid = input("What's your bid?: $")
         bids[user_name] = bid
-        next_user = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+        next_user = input("Are there any other bidders? \
+                           Type 'yes' or 'no'.\n").lower()
         if next_user == 'no':
             bid_values = bids.values()
             highest_bid = max(bid_values)
             index_of_highest_bid = list(bids.values()).index(highest_bid)
             highest_bidder = list(bids.keys())[index_of_highest_bid]
-            print(f'The winner is {highest_bidder} with a bid of ${highest_bid}.')
+            print(f'The winner is {highest_bidder} with a bid of \
+                  ${highest_bid}.')
             break
         elif next_user == 'yes':
             clear()
-            continue    
+            continue
 
-secret_auction()        
+
+secret_auction()
