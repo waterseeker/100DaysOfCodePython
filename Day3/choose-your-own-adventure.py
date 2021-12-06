f2b803b628bc288022be38dcf351d81f3c628fc1
Wayne Burris <waterseeker@Hotmail.com>
2021-12-06 16:15:02 -0600

    working on choose your own adventure

---
+++ Day3/choose-your-own-adventure.py
@@ -0,0 +1,51 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# Write your code below this line 👇
+playing_game = True
+while playing_game:
+    print("You are on a tiny beach that is surrounded by ridiculously high, un\
+        scalable cliffs. After looking around for a while, you notice a large \
+        crack at the base of one of the cliffs. You manage to wriggle your way\
+        through. Once on the other side, you are surrounded by a lush jungle. \
+        There is a path. You can venture left or right.")
+    first_choice_is_valid = False
+    while not first_choice_is_valid:
+        first_choice = input("Which way do you want to go? Left or right? ")
+        cleaned_first_choice = first_choice.lower()
+        if (cleaned_first_choice == "left" or 
+                cleaned_first_choice == "right"):
+            first_choice_is_valid = True
+            break
+        print("Sorry, that's not a valid choice. Please choose left or right.")
+        continue
+    if cleaned_first_choice == "left":
+        print("You come to a small river. ")
+    elif cleaned_first_choice == "right":
+        print("You trip on a vine, stumble, and fall into a pit. Turns out to \
+            be filled with poisonous snakes. So there's that. GAME OVER.")
+        break
