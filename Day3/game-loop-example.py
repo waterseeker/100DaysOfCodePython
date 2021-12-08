8d44d4681751a25e7896c410ec34abc5be849a76
Wayne Burris <waterseeker@Hotmail.com>
2021-12-07 21:04:53 -0600

    first pass of draft game logic

--- Day3/game-loop-example.py
+++ Day3/game-loop-example.py
@@ -1,13 +1,29 @@
-playing_game = True
-while playing_game:
-    choice = input("l or r?")
-    if choice is not "l" and choice is not "r":
-        print("invalid, choose l or r.")
-        # return to beginning of loop
-    if choice == "l":
-        print("continue game")
-    elif choice == "r":
-        print("wrong choice.")
-        playing_game = False
+print("welcome to the game.")
+wants_to_play_is_valid = False
+while not wants_to_play_is_valid:
+    wants_to_play = input("would you like to play? Please enter yes or no. ").lower()
+    if wants_to_play != "yes" and wants_to_play != "no":
+        print("invalid. please choose yes or no.")
+        continue
+    if wants_to_play == "no":
+        wants_to_play_is_valid = True
+        print("Ok. See you later.")
         break
+    elif wants_to_play == "yes":
+        wants_to_play_is_valid = True
+        playing_game = True
+        while playing_game:
+            first_choice_is_valid = False
+            while not first_choice_is_valid:
+                first_choice = input("l or r?")
+                if first_choice != "l" and first_choice != "r":
+                    print("invalid, choose l or r.")
+                    continue
+                if first_choice == "l":
+                    print("continue game")
+                    break
+                elif first_choice == "r":
+                    print("wrong choice.")
+                    playing_game = False
+                    break
 print("GAME OVER")
