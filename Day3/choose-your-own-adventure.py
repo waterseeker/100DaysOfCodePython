8503d7ec8ca41f2a1066f582f01b91524e0caa83
Wayne Burris <waterseeker@Hotmail.com>
2021-12-08 16:33:36 -0600

    basic form of game step function

--- Day3/choose-your-own-adventure.py
+++ Day3/choose-your-own-adventure.py
@@ -27,27 +27,65 @@ print("Your mission is to find the treasure.")
 
 # Write your code below this line 👇
 
-# Decision functions
-def left_or_right(playing_game):
+# Game stage functions
+def first_path_choice(playing_game):
     '''Controls the first decision in the adventure.'''
+
     print("You are on a tiny beach that is surrounded by ridiculously high, un\
         scalable cliffs. After looking around for a while, you notice a large \
         crack at the base of one of the cliffs. You manage to wriggle your way\
         through. Once on the other side, you are surrounded by a lush jungle. \
         There is a path. You can venture left or right.")
+    choice = input("Which way do you want to go? Left or right? ")
+    cleaned_choice = choice.lower().strip()
+    while True:
+        if cleaned_choice != "left" and cleaned_choice != "right":
+            print("Sorry, that's not a valid choice. Please choose left or right.")
+            choice = input("Would you like to go left, or right? ")
+            cleaned_choice = choice.lower().strip()
+            continue
+        if cleaned_choice == "left":
+            print("You come to a small river. ")
+            playing_game = True
+            return playing_game
+        elif cleaned_choice == "right":
+            print("You trip on a vine, stumble, and fall into a pit. Turns out to \
+                be filled with poisonous snakes. So there's that.")
+            playing_game = False
+            return playing_game
+
+def second_path_choice(playing_game):
+    '''Controls the second decision in the adventure.'''
+    if not playing_game:
+        return
+    print("After following the left path for a while you arrive at a wide rive\
+            r. You can try to cross the river now, or make camp for the aftern\
+            oon and try after a nap.")
+    choice = input("Wait or cross? ")
+    cleaned_choice = choice.lower().strip()
+    while True:
+        if cleaned_choice != "wait" and cleaned_choice != "cross":
+            print(f"Huh? What is {cleaned_choice}? Please choose cross or wait.")
+            choice = input("Would you like to go cross or wait? ")
+            cleaned_choice = choice.lower().strip()
+            continue
+        if cleaned_choice == "wait":
+            print("You make camp and have a nice lunch of roasted potatoes and\
+                    carrots with some rosemary and an apple you were delighted\
+                    to find on a nearby tree. After you break camp, you make\
+                    your way across the river and follow a path that leads\
+                    along the shore.")
+            playing_game = True
+            return playing_game
+        elif cleaned_choice == "cross":
+            print("You begin making your way across only to be eaten alive by\
+                    vicious river trout! Who would have guessed?")
+            playing_game = False
+            return playing_game
+
+# Gameplay
 playing_game = True
 while playing_game:
-    first_choice_is_valid = False
-
-    first_choice = input("Which way do you want to go? Left or right? ")
-    cleaned_first_choice = first_choice.lower()
-    if cleaned_first_choice is not "left" and cleaned_first_choice is not "right":
-        print("Sorry, that's not a valid choice. Please choose left or right.")
-
-    if cleaned_first_choice == "left":
-        print("You come to a small river. ")
-    elif cleaned_first_choice == "right":
-        print("You trip on a vine, stumble, and fall into a pit. Turns out to \
-            be filled with poisonous snakes. So there's that. GAME OVER.")
-        break
+    playing_game = first_path_choice(playing_game)
+    playing_game = second_path_choice(playing_game)
 print("GAME OVER")
