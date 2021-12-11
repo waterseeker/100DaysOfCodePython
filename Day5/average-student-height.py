62f600656908fc21aa820e7b9b6fc9c648a728c3
Wayne Burris <waterseeker@Hotmail.com>
2021-12-11 17:43:03 -0600

    first pass of average height calculator

---
+++ Day5/average-student-height.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+# Don't use the len() function, don't use the sum() function
+
+# Write your code below this row 👇
+number_of_students = 0
+total_height = 0
+for height in student_heights:
+    number_of_students += 1
+    total_height += height
+average_height = round(total_height / number_of_students)
+print(average_height)
---
+++ Day5/loops-practice.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    # example of how everything in the for block runs for each entry in the
+    # collection. "Apple Pie" will print before "Peach" does.
+    print(fruit + " Pie")
