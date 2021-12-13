8627c8306fc4b614d0f1d92760dd49030f6788f0
Wayne Burris <waterseeker@Hotmail.com>
2021-12-13 14:36:40 -0600

    first pass hard level password generator

--- Day5/password-generator.py
+++ Day5/password-generator.py
@@ -21,6 +21,11 @@ for number in range(1, nr_symbols + 1):
     password += (random.choice(symbols))
 for number in range(1, nr_numbers + 1):
     password += (random.choice(numbers))
-print(password)
+print(f"Pre-randomized password: {password}")
+
 # Hard Level - Order of characters randomized:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+character_list = list(password)
+random.shuffle(character_list)
+randomized_password = ''.join(character_list)
+print(f"Randomized password: {randomized_password}")
