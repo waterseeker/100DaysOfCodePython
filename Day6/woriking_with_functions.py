7473099d9b715b5dd27164f6fc568f50567434b3
Wayne Burris <waterseeker@Hotmail.com>
2021-12-14 14:07:25 -0600

    reeborgs world hurdle 4

--- Day6/woriking_with_functions.py
+++ Day6/woriking_with_functions.py
@@ -4,3 +4,29 @@ def my_function():
 
 
 my_function()
+
+# reeborg's world hurdle 4 challenge
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump_hurdle():
+#     turn_left()
+#     wall_height = 0
+#     while wall_on_right():
+#         move()
+#         wall_height += 1
+#     turn_right()
+#     move()
+#     turn_right()
+#     while wall_height > 0:
+#         move()
+#         wall_height -= 1
+#     turn_left()
+
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         jump_hurdle()
