fd41042bd522fd236ab447f7f18eb3c750232e2e
Wayne Burris <waterseeker@Hotmail.com>
2021-12-15 13:59:26 -0600

    variable name change

--- Day7/hangman_step_1.py
+++ Day7/hangman_step_1.py
@@ -7,10 +7,10 @@ word_list = ["aardvark", "baboon", "camel"]
 random_word = random.choice(word_list)
 # TODO-2 - Ask the user to guess a letter and assign their answer to a
 # variable called guess. Make guess lowercase.
-player_guess = input("Please choose a letter:\n").lower()
+guess = input("Please choose a letter:\n").lower()
 # TODO-3 - Check if the letter the user guessed (guess) is one of the
 # letters in the chosen_word.
-if player_guess in random_word:
-    print(f"{player_guess} is in {random_word}.")
+if guess in random_word:
+    print(f"{guess} is in {random_word}.")
 else:
-    print(f"{player_guess} is not in {random_word}.")
+    print(f"{guess} is not in {random_word}.")
