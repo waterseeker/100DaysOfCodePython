ec949be6ec65006da1d1a987636b2cf68a0aafef
Wayne Burris <waterseeker@Hotmail.com>
2021-12-16 13:43:53 -0600

    first pass step 4 hangman

--- Day7/hangman_step_4.py
+++ Day7/hangman_step_4.py
@@ -65,6 +65,7 @@ chosen_word = random.choice(word_list)
 # TODO-1: - Create a variable called 'lives' to keep track of the number of
 # lives left.
 # Set 'lives' to equal 6.
+lives = 6
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -74,27 +75,34 @@ display = ['_' for x in chosen_word]
 
 while True:
     guess = input("Guess a letter: ").lower()
-
+    # TODO-2: - If guess is not a letter in the chosen_word,
+    # Then reduce 'lives' by 1.
+    # If lives goes down to 0 then the game should stop and it should print
+    # "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print(display)
+            print(stages[lives])
+            print("You lose.")
+            break
     # Check guessed letter
     index = 0
     for letter in chosen_word:
         if letter == guess:
             display[index] = guess
             index += 1
-    # TODO-2: - If guess is not a letter in the chosen_word,
-    # Then reduce 'lives' by 1.
-    # If lives goes down to 0 then the game should stop and it should print
-    # "You lose."
         else:
             index += 1
 
     # Join all the elements in the list and turn it into a String.
     print(f"{''.join(display)}")
-
+    # TODO-3: - print the ASCII art from 'stages' that corresponds to the
+    # current number of 'lives' the user has remaining.
+    print(stages[lives])
     # Check if user has got all letters.
     if "_" not in display:
         print("You win!")
         break
 
-    # TODO-3: - print the ASCII art from 'stages' that corresponds to the
-    # current number of 'lives' the user has remaining.
+
