c489b161645ff8694a378f728723656b01f6a478
Wayne Burris <waterseeker@Hotmail.com>
2021-12-17 13:17:53 -0600

    first pass of final hangman

--- Day7/hangman_complete/hangman_step_5.py
+++ Day7/hangman_complete/hangman_step_5.py
@@ -3,11 +3,11 @@
 import random
 import hangman_art
 import hangman_words
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-chosen_word = hangman_words.word_list
+
+# Update the word list to use the 'word_list' from hangman_words.py
+chosen_word = random.choice(hangman_words.word_list)
 lives = 6
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of
-# the game.
+# Import the logo from hangman_art.py and print it at the start of the game.
 logo = hangman_art.logo
 print(logo)
 # Testing code
@@ -16,11 +16,20 @@ print(f'Pssst, the solution is {chosen_word}.')
 # Create blanks
 display = ['_' for x in chosen_word]
 
+# Track guesses letters
+guesses = []
 while True:
     guess = input("Guess a letter: ").lower()
+    # If the user has entered a letter they've already guessed, print the
+    # letter and let them know.
+    if guess in guesses:
+        print(f"You've already guessed {guess}. Try again.")
+        continue
+    elif guess not in guesses:
+        guesses.append(guess)
     if guess not in chosen_word:
-        # TODO-5: - If the letter is not in the chosen_word, print out the
-        # letter and let them know it's not in the word.
+        # If the letter is not in the chosen_word, print out the letter and
+        # let them know it's not in the word.
         print(f"Sorry, {guess} is not in the word. You lose a life.")
         lives -= 1
         print(f"You have {lives} lives remaning.")
@@ -29,8 +38,7 @@ while True:
             print(hangman_art.stages[lives])
             print("You lose.")
             break
-    # TODO-4: - If the user has entered a letter they've already guessed,
-    # print the letter and let them know.
+
     # Check guessed letter
     index = 0
     for letter in chosen_word:
@@ -42,9 +50,10 @@ while True:
 
     # Join all the elements in the list and turn it into a String.
     print(f"{''.join(display)}")
-    # TODO-2: - Import the stages from hangman_art.py and make this error go
-    # away.
+
+    # Import the stages from hangman_art.py.
     print(hangman_art.stages[lives])
+
     # Check if user has got all letters.
     if "_" not in display:
         print("You win!")
