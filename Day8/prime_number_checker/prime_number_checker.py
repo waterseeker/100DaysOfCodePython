d6f33e5ae549b46eebd0503224d29fce2f82ae08
Wayne Burris <waterseeker@Hotmail.com>
2021-12-18 13:43:03 -0600

    first pass of prime number checker

--- Day8/prime_number_checker/prime_number_checker.py
+++ Day8/prime_number_checker/prime_number_checker.py
@@ -1,11 +1,21 @@
 # Write your code below this line 👇
 
 
-
-
-
+def prime_checker(number):
+    if number < 2:
+        print("It's a prime number.")
+        return
+    divisor = 3
+    while divisor * divisor <= n:
+        if number % divisor == 0 and divisor != number:
+            print("It's not a prime number.")
+            return
+        divisor += 1
+    if number % number == 0:
+        print("It's a prime number.")
 # Write your code above this line 👆
 
+
 # Do NOT change any of the code below👇
-n = int(input("Check this number: "))
+n = int(input("Check this number"))
 prime_checker(number=n)
