4011f599a2c635666746e10b8be0f194e89bfcfd
Wayne Burris <waterseeker@Hotmail.com>
2021-12-19 20:18:41 -0600

    first pass of caesar cipher 4

--- Day8/caesar_cipher/caesar_cipher_4.py
+++ Day8/caesar_cipher/caesar_cipher_4.py
@@ -1,6 +1,5 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
@@ -9,18 +8,17 @@ def caesar(start_text, shift_amount, cipher_direction):
     if cipher_direction == "decode":
         shift_amount *= -1
     for char in start_text:
-        # TODO-3: What happens if the user enters a number/symbol/space?
-        # Can you fix the code to keep the number/symbol/space when the text is
-        # encoded/decoded?
-        # e.g. start_text = "meet me at 3"
-        # end_text = "•••• •• •• 3"
-        position = alphabet.index(char)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+        if char.isalpha():
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-# TODO-1: Import and print the logo from art.py when the program starts.
+print(logo)
+run_program_again = True
 
 # TODO-4: Can you figure out a way to ask the user if they want to restart the
 # cipher program?
@@ -32,13 +30,20 @@ def caesar(start_text, shift_amount, cipher_direction):
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-# TODO-2: What if the user enters a shift that is greater than the number of
-# letters in the alphabet?
-# Try running the program and entering a shift number of 45.
-# Add some code so that the program continues to work even if the user enters
-# a shift number greater than 26.
-# Hint: Think about how you can use the modulus (%).
+shift = int(input("Type the shift number:\n")) % 26
 
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+while run_program_again:
+    run_again = input("Would you like to restart the cipher program?\n")
+    if run_again == "yes":
+        direction = input("Type 'encode' to encrypt, type 'decode' to \
+decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n")) % 26
+        caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    elif run_again == "no":
+        run_program_again = False
+    else:
+        print(f"Sorry, I don't understand {run_again}. \
+You have to enter either yes or no.")
+        continue
