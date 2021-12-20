b445eb132747afbb3dccdc0ecc6beff732bd7592
Wayne Burris <waterseeker@Hotmail.com>
2021-12-20 13:45:19 -0600

    first pass of grading program

--- Day9/grading_program.py
+++ Day9/grading_program.py
@@ -8,8 +8,21 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 # TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
+for key, value in student_scores.items():
+    student = key
+    if value <= 70:
+        grade = "Fail"
+    elif value >= 71 and value < 81:
+        grade = "Acceptable"
+    elif value >= 81 and value < 91:
+        grade = "Exceeds Expectations"
+    else:
+        grade = "Outstanding"
+    student_grades[student] = grade
+
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
