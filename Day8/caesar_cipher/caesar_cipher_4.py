a659b4fcfc3a403fe9d6f81e3f220e0507842e0b
Wayne Burris <waterseeker@Hotmail.com>
2021-12-20 13:14:39 -0600

    removed todo, first pass done on caesar cipher final

--- Day8/caesar_cipher/caesar_cipher_4.py
+++ Day8/caesar_cipher/caesar_cipher_4.py
@@ -20,14 +20,6 @@ def caesar(start_text, shift_amount, cipher_direction):
 print(logo)
 run_program_again = True
 
-# TODO-4: Can you figure out a way to ask the user if they want to restart the
-# cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call
-# the caesar() function again?
-# Hint: Try creating a while loop that continues to execute the program if the
-# user types 'yes'.
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n")) % 26
