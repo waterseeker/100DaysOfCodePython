07292c5fd9e32d46dc41475280ee8aacee9d8fe9
Wayne Burris <waterseeker@Hotmail.com>
2021-12-21 11:02:07 -0600

    working on re-run logic for next bidder in secret_auction

--- Day9/secret_auction/secret_auction.py
+++ Day9/secret_auction/secret_auction.py
@@ -1,8 +1,38 @@
 import os
+from art import logo
 
 
 def clear():
     lambda: os.system("clear")
 
 
-clear()
+def next_bidder(is_bidding):
+    run_again = input("Is there another bidder?\n").lower()
+    while is_bidding:
+        if run_again == "yes":
+            clear()
+            return
+        elif run_again == "no":
+            is_bidding = False
+            return
+        else:
+            print(f"Sorry, I don't understand {run_again}. You have to choose \
+    yes or no.")
+            next_bidder(is_bidding)
+
+
+print(logo)
+bids = {}
+bidding = True
+while bidding:
+    user_name = input("What is your name?\n")
+    user_bid = int(input("What is your bid?\n"))
+    bids[user_name] = user_bid
+    next_bidder(bidding)
+highest_bid = 0
+highest_bidder = ""
+for user in bids:
+    if bids[user] > highest_bid:
+        highest_bid = bids[user]
+        highest_bidder = user
+print(highest_bid, highest_bidder)
