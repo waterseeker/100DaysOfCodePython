8f3df7da8736b2803b4a0030252cd7cd94b169a9
Wayne Burris <waterseeker@Hotmail.com>
2021-12-22 11:14:26 -0600

    moved bidding logic into recursive function

--- Day9/secret_auction/secret_auction.py
+++ Day9/secret_auction/secret_auction.py
@@ -2,30 +2,26 @@ import os
 from art import logo
 
 
-def next_bidder(is_bidding):
-    run_again = input("Is there another bidder?\n").lower()
-    while is_bidding:
-        if run_again == "yes":
-            os.system('clear')
-            return
-        elif run_again == "no":
-            global bidding
-            bidding = False
+def bidding():
+    user_name = input("What is your name?\n")
+    user_bid = int(input("What is your bid?\n"))
+    bids[user_name] = user_bid
+    while True:
+        run_again = input("Is there another bidder?\n").lower()
+        if run_again == "no":
             break
+        elif run_again == "yes":
+            os.system('clear')
+            return bidding()
         else:
             print(f"Sorry, I don't understand {run_again}. You have to choose \
 yes or no.")
-            next_bidder(is_bidding)
+            continue
 
 
 print(logo)
 bids = {}
-bidding = True
-while bidding:
-    user_name = input("What is your name?\n")
-    user_bid = int(input("What is your bid?\n"))
-    bids[user_name] = user_bid
-    next_bidder(bidding)
+bidding()
 highest_bid = 0
 highest_bidder = ""
 for user in bids:
