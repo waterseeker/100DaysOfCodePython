3d37a553b7b8231acf4d5fb9429df331ee0f5843
Wayne Burris <waterseeker@Hotmail.com>
2021-12-23 13:50:54 -0600

    first pass of days in month

--- Day10/days_in_month/days_in_month.py
+++ Day10/days_in_month/days_in_month.py
@@ -2,17 +2,21 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year.")
+                return True
             else:
-                print("Not leap year.")
+                return False
         else:
-            print("Leap year.")
+            return True
     else:
-        print("Not leap year.")
+        return False
 
 
-def days_in_month():
+def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month == 2 and is_leap(year) is True:
+        return 29
+    else:
+        return month_days[month - 1]
 
 
 # 🚨 Do NOT change any of the code below
