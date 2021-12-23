326c04ea418e63ae6757bbc318df016624b16425
Wayne Burris <waterseeker@Hotmail.com>
2021-12-23 14:03:49 -0600

    added example output

---
+++ Day10/calculator/calculator.py
@@ -0,0 +1,11 @@
+# Example output:
+
+# What's the first number?: 3
+# +
+# -
+# *
+# /
+# Pick an operation: *
+# 3.0 * 5.0 = 15.0
+# Type 'y' to continue calculating with 15.0, or type 'n' to start a new
+# calculation:
