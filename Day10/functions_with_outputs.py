378c9aac0e4434e97e4d18af4598723e9fb00ff4
Wayne Burris <waterseeker@Hotmail.com>
2021-12-23 13:40:42 -0600

    start of days in month

--- Day10/functions_with_outputs.py
+++ Day10/functions_with_outputs.py
@@ -1,6 +1,16 @@
 # Functions with Outputs
 
 
-def format_name():
+def format_name(first_name, last_name):
     '''Takes in a first name and last name and changes them to title case.
     '''
+    if first_name == "" or last_name == "":
+        return "You didn't provide valid inputs."
+    formatted_first_name = first_name.title()
+    formatted_last_name = last_name.title()
+
+    return f"{formatted_first_name} {formatted_last_name}"
+
+
+print(format_name(input("What is your first name? "),
+                  input("What is your last name? ")))
