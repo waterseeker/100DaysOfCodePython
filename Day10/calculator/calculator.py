1f4dc61ef5e78fa343d071013c2008298302889d
Wayne Burris <waterseeker@Hotmail.com>
2021-12-23 14:06:51 -0600

    added step outline

--- Day10/calculator/calculator.py
+++ Day10/calculator/calculator.py
@@ -9,3 +9,20 @@
 # 3.0 * 5.0 = 15.0
 # Type 'y' to continue calculating with 15.0, or type 'n' to start a new
 # calculation:
+
+# get first number input
+# check that it's a number
+#   if not, error message
+#   if so, continue
+# get the operator
+# check operator is valid
+#   if not, error message
+#   if so, continue
+# get second number
+# check second number is valid
+#   if not, error message
+#   if so, continue
+# print out equation with the answer
+# get input to continue with answer or not
+#   if not, exit program
+#   if so, return to selecting operation and continue from there
