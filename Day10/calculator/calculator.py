6a38990b10b7edea834cfd8a17b0f22634368e6a
Wayne Burris <waterseeker@Hotmail.com>
2021-12-24 13:56:07 -0600

    working on regex logic for checking user input for operator

--- Day10/calculator/calculator.py
+++ Day10/calculator/calculator.py
@@ -9,20 +9,65 @@
 # 3.0 * 5.0 = 15.0
 # Type 'y' to continue calculating with 15.0, or type 'n' to start a new
 # calculation:
+import re
 
-# get first number input
-first_number = float(input("What's the first number?\n"))
-# check that it's a number (make this a function)
-try:
-    if float(i).is_integer():
-        print("integer")
+
+def get_number(user_input):
+    """Takes a user string input, sees if it's castable to a number or float.
+       Prints and error message if not and calls itself. Otherwise returns the
+       number or float.
+    """
+    # Int is:
+    #  - Only numbers that do NOT start with 0 (protect padded number strings)
+    #  - Exactly 0
+    re_int = re.compile(r"(^[1-9]+\d*$|^0$)")
+    # Float is:
+    #  - Only numbers but with exactly 1 dot.
+    #  - The dot must always be followed number numbers
+    re_float = re.compile(r"(^\d+\.\d+$|^\.\d+$)")
+    if re_int.match(user_input):
+        first_number = int(user_input)
+        return first_number
+    elif re_float.match(user_input):
+        first_number = float(user_input)
+        return first_number
     else:
-        print("float")
-except ValueError:
-    print("not integer or float")
-#   if not, error message
-#   if so, continue
-# get the operator
+        print("Sorry, that's not a number. Try again.")
+        user_input = input("What's the first number?\n")
+        return get_number(user_input)
+
+
+def get_operator(user_input):
+    """Takes in a string and checks that it is either +, -, *, or /. Returns
+       True if it is and False if it's not.
+    """
+    input_length = len(user_input)
+    if input_length == 1:
+        # Operator is:
+        #  +, -, *, or /
+        re_operator = re.compile(r"(+-*/)")
+        if re_operator.match(user_input):
+            print("Match")
+        else:
+            print("No Match")
+    else:
+        print("Sorry, that's not a valid operator. Please choose +, -, *, or \
+/")
+        user_input = input("Pick an operation.\n")
+        get_operator(user_input)
+
+
+user_input = input("Pick an operation.\n")
+get_operator(user_input)
+
+# if len(x) == 1 and get_operator(x):
+#     print("Yep")
+# else:
+#     print("nah")
+
+# user_input = input("What's the first number?\n")
+# first_number = get_number(user_input)
+# get the operator (make this a function)
 # check operator is valid
 #   if not, error message
 #   if so, continue
