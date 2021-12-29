509193255e461fe5775d30a2294439a5a8542935
Wayne Burris <waterseeker@Hotmail.com>
2021-12-29 13:54:58 -0600

    first pass of ace logic

--- Day11/blackjack.py
+++ Day11/blackjack.py
@@ -39,28 +39,30 @@ def draw_card(hand):
     """Adds 1 card to the hand."""
     hand.append(random.choice(cards))
 
-# You've most likely got a method that evaluates a blackjack hand value.
-# Always value aces at one point. If a hand contains an ace, compute a hard
-# value (all aces are ones, +10) and a soft value (all aces ones).
 
-# If the hard value is not a bust, return the hard value. If the hard value is
-# a bust, return the soft value.
+def score_hand(hand):
+    """Takes in a hand array and returns the best score possible with the
+       given cards."""
 
-
-def evaluate_hand(hand):
-    """Takes in a hand array and returns the sum, 'blackjack' if it's blackjack
-       and 'bust' if it's a bust."""
-    # all aces are ones, +10
-    value_with_ace_as_11 = sum(hand)
+    # all aces are ones, then add 10 for one to count as 11
+    if 11 in hand:
+        score_with_ace_as_11 = sum([card if card != 11 else 1
+                                    for card in hand]) + 10
+    else:
+        score_with_ace_as_11 = 0
     # all aces are ones
-    value_with_ace_as_1 = sum(hand)
-    return value_with_11
+    score_with_ace_as_1 = sum([card if card != 11 else 1 for card in hand])
+    scores = [score_with_ace_as_11, score_with_ace_as_1]
+    if score_with_ace_as_11 > 21 and score_with_ace_as_1 > 21:
+        return min(scores)
+    else:
+        return max(scores)
 
 
 def show_hands():
     """Prints out the player's hand and the first card of the dealer's hand."""
     print(f"Your cards: {players_hand}, current score: \
-{evaluate_hand(players_hand)}")
+{score_hand(players_hand)}")
     print(f"Computer's first card: {dealers_hand[0]}")
 
 
@@ -71,7 +73,8 @@ while play_game:
     draw_card(dealers_hand)
     draw_card(players_hand)
     draw_card(players_hand)
-    show_player_hand()
-    show_dealer_hand()
+    show_hands()
+    if score_hand(dealers_hand) == 21 and score_hand(players_hand) != 21:
+        print("Dealer has blackjack!\n You lose :(")
     # setting play_game to False to break the loop
     play_game = False
