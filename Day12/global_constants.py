0d5035fde00d9a537dc4788788bd247a1a2b2eb8
Wayne Burris <waterseeker@Hotmail.com>
2022-01-03 13:28:33 -0600

    added examples of global constants

---
+++ Day12/global_constants.py
@@ -0,0 +1,7 @@
+# global constants are variables that you define and are never planning on
+# changing again.
+
+# The naming convention for constants in Python is to make the name all
+# caps, separated with underscores
+PI = 3.14159
+SOMETHING_ELSE = "blah blah"
