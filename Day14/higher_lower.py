90a8817e17e1b69b42fca8b8a4dfd01cf5b7d6b8
Wayne Burris <waterseeker@Hotmail.com>
2022-01-06 13:40:27 -0600

    start of higher lower game

---
+++ Day14/higher_lower.py
@@ -0,0 +1,5 @@
+# higher lower logo art on game start
+# "Compare A: Christiana Ronaldo, a Footballer, from Portugal"
+# vs Art
+# "Against B: Vin Diesel, a Actor, from United States.
+# Who has more followers? Type'A' or 'B':
