40e7d26e5838980260070a6b7588b05be68f7380
Wayne Burris <waterseeker@Hotmail.com>
2022-01-13 13:26:56 -0600

    working on coffee machine

--- Day15/coffee_machine.py
+++ Day15/coffee_machine.py
@@ -29,3 +29,28 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+def get_drink_choice():
+    """Gets user input to select a drink. Returns the drink item from the
+    MENU. Returns False if user inputs 'off'"""
+    drink_choice = input("What would you like? (espresso/latte/cappuccino):\n")
+    cleaned_drink_choice = drink_choice.strip().lower()
+    if cleaned_drink_choice == 'off':
+        return False
+    elif cleaned_drink_choice not in MENU:
+        print("Please select espresso, latte, or cappuccino.")
+        return get_drink_choice()
+    else:
+        return cleaned_drink_choice
+
+
+def get_drink_item(drink_name):
+    """Takes in a string, returns item with that key from the MENU"""
+    return MENU[drink_name]
+
+
+# testing code
+choice = get_drink_choice()
+drink = get_drink_item(choice)
+print(drink['ingredients']["water"])
