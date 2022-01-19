25b671de034883160f96d86bf8224712aa040be1
Wayne Burris <waterseeker@Hotmail.com>
2022-01-19 14:53:25 -0600

    start of dashed line challenge

---
+++ Day18/dashed_line.py
@@ -0,0 +1,4 @@
+from turtle import Turtle()
+
+# Challenge: draw a dashed line. solid for 10, empty for 10, repeat this 50
+# times.
--- Day18/main.py
+++ Day18/main.py
@@ -5,18 +5,5 @@ timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("springgreen4")
 timmy_the_turtle.forward(100)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 screen = Screen()
 screen.exitonclick()
