37694030a949bcee6188449a3b1d144ef0a1e48d
Wayne Burris <waterseeker@Hotmail.com>
2022-01-19 14:57:03 -0600

    first pass of dashed line challenge

--- Day18/dashed_line.py
+++ Day18/dashed_line.py
@@ -1,4 +1,15 @@
-from turtle import Turtle()
+from turtle import Turtle, Screen
 
 # Challenge: draw a dashed line. solid for 10, empty for 10, repeat this 50
 # times.
+tim = Turtle()
+tim.shape("turtle")
+tim.color("forestgreen")
+for _ in range(50):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+screen = Screen()
+screen.exitonclick()
