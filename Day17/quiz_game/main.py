c5d0155a2cabb18a49428a98c39b565e085f3e05
Wayne Burris <waterseeker@Hotmail.com>
2022-01-19 10:24:06 -0600

    example of changing the data with just a few changes to main.py

--- Day17/quiz_game/main.py
+++ Day17/quiz_game/main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-question_bank = [Question(question["text"], question["answer"])
+question_bank = [Question(question["question"], question["correct_answer"])
                  for question in question_data]
 
 quiz = QuizBrain(question_bank)
