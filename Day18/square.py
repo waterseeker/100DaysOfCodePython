3c0ef224e520ec81db43800330dd514d06059556
Wayne Burris <waterseeker@Hotmail.com>
2022-01-19 12:53:54 -0600

    draw a square challenge complete

--- Day18/square.py
+++ Day18/square.py
@@ -3,20 +3,11 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("springgreen4")
-timmy_the_turtle.forward(100)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Challenge: Draw a 100 x 100 square.
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
