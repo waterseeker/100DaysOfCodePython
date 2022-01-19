ff676757ece6d898efa11d34deff28eae3ed3f17
Wayne Burris <waterseeker@Hotmail.com>
2022-01-19 15:01:41 -0600

    start of different shapes

---
+++ Day18/different_shapes.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+# Challenge draw a 3 sided shape with 100 len sides, then 4 sided with 100 len
+# sides, then 5, sided, etc.
+
+tia = Turtle()
+tia.shape("turtle")
+tia.color("purple")
+
+screen = Screen()
+screen.exitonclick()
