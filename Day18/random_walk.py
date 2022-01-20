1f4175969037e8ab301e58bbd864b6c3769ea8ba
Wayne Burris <waterseeker@Hotmail.com>
2022-01-20 14:56:03 -0600

    added a method for generating a randomized tuple for color choice, changed import of turtle for better readablility

--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -1,24 +1,32 @@
-from turtle import Turtle, Screen, colormode
-from random import randint, choice
+import turtle as t
+import random
 # move the turtle in increments of 10
 # each move should be randomized to be up, down, left, or right.
 # make the turtle move faster than normal
 # make the lines the turtle draws thicker than normal
 # make each moves line a random color
-geoff = Turtle()
-geoff.pensize(3)
+geoff = t.Turtle()
+geoff.pensize(10)
 geoff.speed(0)
-colormode(255)
+t.colormode(255)
+
+
+def random_color():
+    """Returns a tuple of 3 random ints from 0 to 255"""
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 
 def move_turtle(turtle, duration):
     for _ in range(duration):
-        turtle.color(randint(0, 255), randint(0, 255), randint(0, 255))
+        turtle.color(random_color())
         angles = [0, 90, 180, 270]
-        turtle.right(choice(angles))
+        turtle.right(random.choice(angles))
         turtle.forward(10)
 
 
 move_turtle(geoff, 500)
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
