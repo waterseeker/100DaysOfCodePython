4a04f8f59673aa26f93d8154e23bea40bde00d71
Wayne Burris <waterseeker@Hotmail.com>
2022-01-20 14:29:46 -0600

    start of random walk

---
+++ Day18/random_walk.py
@@ -0,0 +1,5 @@
+# move the turtle in increments of 10
+# each move should be randomized to be up, down, left, or right.
+# make the turtle move faster than normal
+# make the lines the turtle draws thicker than normal
+# make each moves line a random color
