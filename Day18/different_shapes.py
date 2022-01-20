bf33e3e7f3cad9b47b1b6527431c83137b230f6f
Wayne Burris <waterseeker@Hotmail.com>
2022-01-20 14:24:24 -0600

    completed different shapes challenge

--- Day18/different_shapes.py
+++ Day18/different_shapes.py
@@ -1,10 +1,23 @@
-from turtle import Turtle, Screen
-# Challenge draw a 3 sided shape with 100 len sides, then 4 sided with 100 len
-# sides, then 5, sided, etc.
+from turtle import Turtle, Screen, colormode
+from random import randint
+# Challenge draw a triangle, square, pentagon, hexagon, heptagon, octagon,
+# nonagon, and decagon. Each side should be 100 in length and a random color.
+
+# each shape has angles = 360/#of sides
 
 tia = Turtle()
 tia.shape("turtle")
-tia.color("purple")
+colormode(255)
+
+sides = 3
+while sides <= 10:
+    # random color
+    tia.color(randint(0, 255), randint(0, 255), randint(0, 255))
+    for _ in range(sides):
+        degrees = 360/sides
+        tia.forward(100)
+        tia.right(degrees)
+    sides += 1
 
 screen = Screen()
 screen.exitonclick()
