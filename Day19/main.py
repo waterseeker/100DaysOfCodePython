4342ff09e201d24d1f65c51881a7f68c183c247d
Wayne Burris <waterseeker@Hotmail.com>
2022-01-22 15:20:22 -0600

    start of etch a sketch challenge

--- Day19/main.py
+++ Day19/main.py
@@ -1,1 +1,19 @@
+from turtle import Turtle, Screen
+# CHALLENGE:
+# create a turtle that will allow you to:
+#   press the w key to go forwards
+#             s key to go backwards
+#             a key to go left
+#             d key to go right
+#             c key to clear the drawing and put the turtle back in the center
+bella = Turtle()
+screen = Screen()
 
+
+def move_forwards():
+    bella.forward(10)
+
+
+screen.listen()
+screen.onkey(key='space', fun=move_forwards)
+screen.exitonclick()
