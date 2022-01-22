22f41aaa3670e9f0a8eddb684a38c0dd5c16018d
Wayne Burris <waterseeker@Hotmail.com>
2022-01-22 15:31:35 -0600

    first pass of etch a sketch

--- Day19/main.py
+++ Day19/main.py
@@ -1,4 +1,6 @@
+import turtle
 from turtle import Turtle, Screen
+
 # CHALLENGE:
 # create a turtle that will allow you to:
 #   press the w key to go forwards
@@ -14,6 +16,28 @@ def move_forwards():
     bella.forward(10)
 
 
+def move_backwards():
+    bella.backward(10)
+
+
+def turn_left():
+    bella.left(10)
+
+
+def turn_right():
+    bella.right(10)
+
+
+def reset():
+    bella.clear()
+    bella.reset()
+
+
 screen.listen()
-screen.onkey(key='space', fun=move_forwards)
+screen.onkey(key='w', fun=move_forwards)
+screen.onkey(key='a', fun=move_backwards)
+screen.onkey(key='s', fun=turn_left)
+screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='c', fun=reset)
+
 screen.exitonclick()
