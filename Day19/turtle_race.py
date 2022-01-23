f7a003a3526e7a125baf4129af222800d7823a7b
Wayne Burris <waterseeker@Hotmail.com>
2022-01-23 10:09:24 -0600

    first pass of turtle racing game

--- Day19/turtle_race.py
+++ Day19/turtle_race.py
@@ -1,24 +1,38 @@
 from turtle import Turtle, Screen
+import random
 
-
+is_race_running: False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Bettin' Time!",
                             prompt="Which turtle do you think is going to win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 turtles = []
-# tian = Turtle(shape="turtle")
-# tian.penup()
-# tian.goto(x=-230, y=-100)
 
 x = -230
 y = -100
 
 for color_from_list in colors:
-    turtle = Turtle(shape="turtle")
-    turtle.color(color_from_list)
-    turtle.penup()
-    turtle.goto(x, y)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color_from_list)
+    new_turtle.penup()
+    new_turtle.goto(x, y)
     y += 50
+    turtles.append(new_turtle)
+
+if user_bet:
+    is_race_running = True
+
+while is_race_running:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_running = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
