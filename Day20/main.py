9464fa6fc8c50832cecbb720f1638f78c9c3c967
Wayne Burris <waterseeker@Hotmail.com>
2022-01-24 09:37:00 -0600

    added snake class

--- Day20/main.py
+++ Day20/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,26 +11,14 @@ screen.tracer(0)
 # create a snake body that is 3 turtle objects, each 20 X 20 white squares
 # the first one is at 0, 0.
 # each other one is 20px to the left
-x_coordinate = 0
-y_coordinate = 0
-snake_segments = []
-for _ in range(3):
-    snake_segment = Turtle(shape="square")
-    snake_segment.color("white")
-    snake_segment.penup()
-    snake_segment.goto(x_coordinate, y_coordinate)
-    snake_segments.append(snake_segment)
-    x_coordinate += -20
+
+snake = Snake()
 
 game_is_running = True
 while game_is_running:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
-    for segment_number in range(len(snake_segments) - 1, 0, -1):
-        new_x_coordinate = snake_segments[segment_number - 1].xcor()
-        new_y_coordinate = snake_segments[segment_number - 1].ycor()
-        snake_segments[segment_number].goto(new_x_coordinate, new_y_coordinate)
-    snake_segments[0].forward(20)
 
 screen.exitonclick()
---
+++ Day20/snake.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Snake():
+    def __init__(self):
+        self.x_coordinate = 0
+        self.y_coordinate = 0
+        self.snake_segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for _ in range(3):
+            snake_segment = Turtle(shape="square")
+            snake_segment.color("white")
+            snake_segment.penup()
+            snake_segment.goto(self.x_coordinate, self.y_coordinate)
+            self.snake_segments.append(snake_segment)
+            self.x_coordinate += -20
+
+    def move(self):
+        for segment_number in range(len(self.snake_segments) - 1, 0, -1):
+            new_x_coordinate = self.snake_segments[segment_number - 1].xcor()
+            new_y_coordinate = self.snake_segments[segment_number - 1].ycor()
+            self.snake_segments[segment_number].goto(new_x_coordinate, new_y_coordinate)
+        self.snake_segments[0].forward(20)
