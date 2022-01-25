4ebb5b180c8ffa7da882ab6d16cfbe9a52772baa
Wayne Burris <waterseeker@Hotmail.com>
2022-01-25 14:56:37 -0600

    ball collision with paddles

--- Day22/ball.py
+++ Day22/ball.py
@@ -9,8 +9,16 @@ class Ball(Turtle):
         self.penup()
         self.setposition(0, 0)
         self.right(45)
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- Day22/main.py
+++ Day22/main.py
@@ -24,5 +24,11 @@ while game_is_running:
     time.sleep(.05)
     screen.update()
     ball.move()
+    # collision with top of screen
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
