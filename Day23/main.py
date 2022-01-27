01fc19c1abcea2a54dcbe258b4eb18759902408c
Wayne Burris <waterseeker@Hotmail.com>
2022-01-27 15:37:29 -0600

    added player turtle

--- Day23/main.py
+++ Day23/main.py
@@ -7,6 +7,11 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+
+screen.onkey(player.move_up, "Up")
 
 game_is_on = True
 while game_is_on:
--- Day23/player.py
+++ Day23/player.py
@@ -1,9 +1,27 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+
+
 # The turtle the player controls
 # it moves forward when the player presses the "Up" arrow key
-# if can only move forward
+# it can only move forward
 
-class Player:
-    pass
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.reset_position()
+        self.setheading(90)
+
+    def move_up(self):
+        self.sety(self.ycor() + MOVE_DISTANCE)
+
+    def reset_position(self):
+        self.setposition(STARTING_POSITION)
