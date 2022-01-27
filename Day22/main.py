0835c3004bea715b6f176723d448ba73d8590ca2
Wayne Burris <waterseeker@Hotmail.com>
2022-01-27 15:27:53 -0600

    day 22

--- Day22/main.py
+++ Day22/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 l_paddle = Paddle(-350, 0)
 r_paddle = Paddle(350, 0)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.onkey(l_paddle.move_up, "w")
 screen.onkey(l_paddle.move_down, "s")
@@ -21,18 +23,27 @@ screen.onkey(r_paddle.move_down, "Down")
 
 game_is_running = True
 while game_is_running:
-    time.sleep(.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-    # collision with top of screen
+
+    # collision with top or bottom of screen
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
+
     # collision with paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-#     collision with left or right edge of the screen
-    if ball.xcor() > 380 or ball.xcor() < -380:
-        ball.goto(0, 0)
+
+#     collision with right edge of the screen
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+#     collision with left edge of the screen
+    if ball.xcor() < -380:
+        ball.reset_position()
         ball.bounce_x()
+        scoreboard.r_point()
 
 screen.exitonclick()
