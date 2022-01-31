74a5ee01eab1574f88644973cb44cc7ecf8ecf08
Wayne Burris <waterseeker@Hotmail.com>
2022-01-31 14:10:02 -0600

    added examples of reading and writing files

---
+++ UsingFiles/main.py
@@ -0,0 +1,24 @@
+# read a file
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# # overwrite a file
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+#
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# # append a file
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nSome appended text.")
+#
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# If you try to open a file in write mode that doesn't exist, Python will create that file.
+with open("new_file.txt", mode="w") as file:
+    file.write("This file was created.")
