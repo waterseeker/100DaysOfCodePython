fd019885ec6cc234cfd9dfef7aad8d3f2b09f4cd
Wayne Burris <waterseeker@Hotmail.com>
2022-01-31 14:32:44 -0600

    added high score reading and writing

--- Day24/scoreboard.py
+++ Day24/scoreboard.py
@@ -17,14 +17,19 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
+        self.update_high_score()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def update_high_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.score))
