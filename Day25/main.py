ba1283679f77192627293114d1f2aeefca819ae6
Wayne Burris <waterseeker@Hotmail.com>
2022-02-02 09:29:30 -0600

    start of challenge 2

--- Day25/main.py
+++ Day25/main.py
@@ -1,5 +1,15 @@
-# Challenge: Open up the weather_data.csv file with open and save each line in a list called data
-with open("weather_data.csv") as file:
-    data = file.readlines()
-    stripped_data = [line.strip() for line in data]
-    print(stripped_data)
+# # Challenge: Open up the weather_data.csv file with open and save each line in a list called data
+# with open("weather_data.csv") as file:
+#     data = file.readlines()
+#     stripped_data = [line.strip() for line in data]
+#     print(stripped_data)
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    for row in data:
+        print(row)
+
+# Challenge: using the csv.reader() create a new list called temperatures that contains all of the temps in
+# weather_data.csv as ints not strings
