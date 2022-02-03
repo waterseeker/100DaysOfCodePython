fa4f43ed48829b38cf4977ab0edaef5e1ea5e15b
Wayne Burris <waterseeker@Hotmail.com>
2022-02-03 10:50:33 -0600

    isolated unique fur colors, removed rows with no fur color

--- SquirrelCensus/main.py
+++ SquirrelCensus/main.py
@@ -4,3 +4,12 @@
 #       fur color (this is logged as "Primary Fur Color" in the census data
 #       total for each fur color
 # You should create the final csv file from a DataFrame
+import pandas
+
+# make a dict where the fur colors are the keys and the total squirrels with that color of fur are the values
+squirrel_data = pandas.read_csv("squirrel_data.csv")
+# drop rows with NaN
+only_fur_colors = squirrel_data["Primary Fur Color"].dropna()
+# get only unique vales
+unique_fur_colors = only_fur_colors.unique()
+print(unique_fur_colors)
