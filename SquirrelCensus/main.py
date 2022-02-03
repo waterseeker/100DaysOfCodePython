53851950b160799511b92be9652850f973a370a3
Wayne Burris <waterseeker@Hotmail.com>
2022-02-03 10:40:58 -0600

    start of squirrel census challenge

---
+++ SquirrelCensus/main.py
@@ -0,0 +1,6 @@
+# Goal:
+# Use pandas to:
+#   create a csv from the squirrel data names 'squirrel_count.csv' that has a small table that contains:
+#       fur color (this is logged as "Primary Fur Color" in the census data
+#       total for each fur color
+# You should create the final csv file from a DataFrame
