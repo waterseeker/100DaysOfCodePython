d9d1875b251d5cc2169ddd643dbcd3df1213319d
Wayne Burris <waterseeker@Hotmail.com>
2022-02-04 10:06:00 -0600

    set image as a turtle shape, screen setup

--- 50USStates/main.py
+++ 50USStates/main.py
@@ -1,1 +1,13 @@
+import turtle
 import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+# You can import an image as a turtle shape
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+screen.exitonclick()
