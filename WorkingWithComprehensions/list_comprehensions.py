9e4d04b52744c15bdec6a59b1dc6ce7969c8b213
Wayne Burris <waterseeker@Hotmail.com>
2022-02-05 14:57:52 -0600

    added example of using a conditional statement with a list comprehension

--- WorkingWithComprehensions/list_comprehensions.py
+++ WorkingWithComprehensions/list_comprehensions.py
@@ -15,3 +15,12 @@ print(new_list)
 numbers = [1, 2, 3]
 comprehension = [n + 1 for n in numbers]
 print(comprehension)
+
+# You can add conditional statements to a comprehension as well
+# the format is:
+# new_list = [new_item for item in list if condition]
+
+# get only names that have 4 letters or less
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+new_names = [name for name in names if len(name) <= 4]
+print(new_names)
