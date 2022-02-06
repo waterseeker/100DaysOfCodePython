cb70e28b411eeb8095648debfdd70a3165218f60
Wayne Burris <waterseeker@Hotmail.com>
2022-02-06 15:12:12 -0600

    first pass of working with other files in comprehensions

--- WorkingWithComprehensions/other_files_with_comprehensions.py
+++ WorkingWithComprehensions/other_files_with_comprehensions.py
@@ -26,3 +26,11 @@
 # Example Output
 # [3, 6, 5, 33, 12, 7, 42, 13]
 # [3, 6, 5, 33, 12, 7, 42, 13]
+
+with open("file1.txt") as file1:
+    file1_list = [int(num) for num in file1.readlines()]
+with open("file2.txt") as file2:
+    file2_list = [int(num) for num in file2.readlines()]
+
+result = [num for num in file1_list if num in file2_list]
+print(result)
