5dce41d13644aaf9ab23981ef19eebb2ae5f19e9
Wayne Burris <waterseeker@Hotmail.com>
2022-02-06 15:09:53 -0600

    start of using other files with comprehensions

---
+++ WorkingWithComprehensions/other_files_with_comprehensions.py
@@ -0,0 +1,28 @@
+# Instructions
+# Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+#
+# You are going to create a list called result which contains the numbers that are common in both files.
+#
+# e.g. if file1.txt contained:
+#
+# 1
+# 1
+# 2
+# 2
+# 3
+# 3
+# and file2.txt contained:
+#
+# 2
+# 2
+# 3
+# 3
+# 4
+# 4
+# result = [2, 3]
+# result = [2, 3]
+# IMPORTANT: The result should be a list that contains Integers, not Strings. Try to use List Comprehension instead of a Loop.
+#
+# Example Output
+# [3, 6, 5, 33, 12, 7, 42, 13]
+# [3, 6, 5, 33, 12, 7, 42, 13]
