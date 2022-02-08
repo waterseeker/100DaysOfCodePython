a879a58d009b5a77a4ffe56f5bb8ddfde0cd8ccc
Wayne Burris <waterseeker@Hotmail.com>
2022-02-08 09:43:40 -0600

    finished challenge

--- Day27/working_with_*args.py
+++ Day27/working_with_*args.py
@@ -1,2 +1,9 @@
 # Challenge:
 # Create a function called add that will take any number of arguments, add them all up, and return the sum
+
+
+def add(*args):
+    return sum([num for num in args])
+
+
+print(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
