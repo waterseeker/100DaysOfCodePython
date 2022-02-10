4b8b8b951f54357816f959acce085d4f70a83251
Wayne Burris <waterseeker@Hotmail.com>
2022-02-10 09:05:26 -0600

    added notes for component placement, added timer label

--- PomodoroTimer/main.py
+++ PomodoroTimer/main.py
@@ -9,7 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+CHECK_MARK = "✅"
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -25,9 +25,15 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(
     file="tomato.png")  # canvas.create_image expects a PhotoImage, so you have to load the png like this first
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-
-
+# you set the color of a label by using fg=blah instead of bg=blah
+# "Timer" label at column 1, row 0
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 45), bg=YELLOW)
+timer_label.grid(column=1, row=0)
+# tomato is at column 1, row 1
+# Start button is at column 0, row 2
+# Reset button is at column 2, row 2
+# Checkmarks label is at column 1, row 3
 window.mainloop()
