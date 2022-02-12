aceaeaabeded7b757537287d2dfbe74dcb8c045f
Wayne Burris <waterseeker@Hotmail.com>
2022-02-12 11:18:37 -0600

    added save functionality

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,8 +1,31 @@
 from tkinter import *
 
+user_data = []
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save():
+    global user_data
+    website = website_input.get()
+    username = email_username_input.get()
+    password = password_input.get()
+
+    # save user info
+    user_data.append(f"{website} | {username} | {password}\n")
+    with open("data.txt", 'w') as data:
+        for line in user_data:
+            data.write(line)
+    # clear input fields
+    website_input.delete(0, END)
+    email_username_input.delete(0, END)
+    password_input.delete(0, END)
+    # reset focus and repopulate email
+    website_input.focus()
+    email_username_input.insert(0, "test@test.com")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -18,11 +41,13 @@ website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 website_input = Entry(width=35)
 website_input.grid(column=1, row=1, columnspan=2)  # columnspan will let you make an element span more than 1 column
+website_input.focus()  # sets the cursor to be here when the app launches
 
 email_username_label = Label(text="Email/Username:")
 email_username_label.grid(column=0, row=2)
 email_username_input = Entry(width=35)
 email_username_input.grid(column=1, row=2, columnspan=2)
+email_username_input.insert(0, "test@test.com")  # .insert() lets you set an initial value in the field.
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
@@ -31,7 +56,7 @@ password_input.grid(column=1, row=3)
 generate_button = Button(text="Generate Password")
 generate_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
