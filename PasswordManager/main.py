467b7d2d3dd1439788349cdbe413f628eac721f8
Wayne Burris <waterseeker@Hotmail.com>
2022-02-12 10:42:33 -0600

    ui layout

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -7,7 +7,29 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo = PhotoImage(
     file="logo.png")  # canvas.create_image expects a PhotoImage, so you have to load the png like this first
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_username_label = Label(text="Email/Username:")
+email_username_label.grid(column=0, row=2)
+email_username_input = Entry(width=35)
+email_username_input.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_input = Entry(width=21)
+password_input.grid(column=1, row=3)
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+# columnspan= will let you make an element span more than one column
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
