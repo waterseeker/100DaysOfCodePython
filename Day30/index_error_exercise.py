804b5be9f4eb6cd7a5e9cf4e1e5e01502a282ca3
Wayne Burris <waterseeker@Hotmail.com>
2022-02-14 09:34:13 -0600

    first pass of index error exercise

--- Day30/index_error_exercise.py
+++ Day30/index_error_exercise.py
@@ -3,8 +3,12 @@ fruits = ["Apple", "Pear", "Orange"]
 
 # TODO: Catch the exception and make sure the code runs without crashing.
 def make_pie(index):
-    fruit = fruits[index]
-    print(fruit + " pie")
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
 
 
 make_pie(4)
