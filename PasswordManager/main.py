67a95b5166afba89e687addb7a6eb311f04a5dcb
Wayne Burris <waterseeker@Hotmail.com>
2022-02-13 19:40:45 -0600

    added random password generator code

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,8 +1,37 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+random_letters = [random.choice(letters) for letter in range(nr_letters)]
+password_list += random_letters
+
+random_symbols = [random.choice(symbols) for symbol in range(nr_symbols)]
+password_list += random_symbols
+
+random_numbers = [random.choice(numbers) for number in range(nr_numbers)]
+password_list += random_numbers
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
