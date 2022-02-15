5645fac541aa6a0c5c72566c756c97d3bd891576
Wayne Burris <waterseeker@Hotmail.com>
2022-02-15 10:01:29 -0600

    added error handling

--- NatoAlphabet/main.py
+++ NatoAlphabet/main.py
@@ -1,15 +1,21 @@
-# TODO 1. Create a dictionary in this format: basically convert the csv into a dict
-# {"A": "Alfa", "B": "Bravo"}
-with open("nato_phonetic_alphabet.csv") as data:
-    line_list = [line.strip().split(',') for line in data.readlines()]
-    code_dictionary = {line[0]: line[1] for line in line_list[1:]}
+import pandas
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# the user gets a prompt asking them "Enter a word:"
-while True:
-    user_input = input("Enter a word:\n").upper()
-    # they get a list of the code words printed out for the word they entered
-    # ex. Enter a word: Thomas
-    # ["Tango", "Hotel", "Oscar", "Mike", "Alfa", "Sierra"]
-    nato_code_list = [code_dictionary[letter] for letter in user_input]
-    print(nato_code_list)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dictionary)
+
+
+def encode_word():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(output_list)
+    finally:
+        encode_word()
+
+
+encode_word()
