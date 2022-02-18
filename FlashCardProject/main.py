404aa826290b3b8e803195668133f3e09ca2b764
Wayne Burris <waterseeker@Hotmail.com>
2022-02-18 10:01:22 -0600

    first pass of ui setup

--- FlashCardProject/main.py
+++ FlashCardProject/main.py
@@ -1,2 +1,32 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+# UI SETUP
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+canvas = Canvas(height=526, width=800)
+front_img = PhotoImage(file="images/card_front.png")
+back_img = PhotoImage(file="images/card_back.png")
+canvas.create_image(400, 263, image=front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+# Language text
+canvas.create_text(
+    400, 150,
+    font="Ariel 40 italic",
+    text="Finnish")
+# Word text
+canvas.create_text(
+    400, 263,
+    font="Ariel 60 bold",
+    text="on")
+incorrect_image = PhotoImage(file="images/wrong.png")
+incorrect_button = Button(image=incorrect_image, highlightthickness=0)
+incorrect_button.grid(row=1, column=0)
+correct_image = PhotoImage(file="images/right.png")
+correct_button = Button(image=correct_image, highlightthickness=0)
+correct_button.grid(row=1, column=1)
+
+window.mainloop()
