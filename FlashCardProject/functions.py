54f4b52c77fe4cbe19656837c8524bda06ef8e24
Wayne Burris <waterseeker@Hotmail.com>
2022-02-20 12:06:00 -0600

    getting random word, working on clearing canvas when a new word is generated

--- FlashCardProject/functions.py
+++ FlashCardProject/functions.py
@@ -8,11 +8,11 @@ def get_word_list(number_of_words):
     words = pd.read_csv("data/finnish_words.csv")
     # get a df of number_of_words in length
     words_head = words.head(number_of_words)
-    words_dictionary = words_head.to_dict()
+    words_dictionary = words_head.to_dict(orient="records")
     return words_dictionary
 
 
 # get random word from word dictionary
 def get_random_word(word_dictionary):
-    word_data = random.choice(word_dictionary.items())
+    word_data = random.choice(word_dictionary)
     return word_data
--- FlashCardProject/main.py
+++ FlashCardProject/main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from functions import get_word_list, get_random_word
 
 BACKGROUND_COLOR = "#B1DDC6"
+words_dict = get_word_list(50)
 
 # UI SETUP
 window = Tk()
@@ -21,21 +22,26 @@ language_text = canvas.create_text(
     fill="black",
     font=("Ariel", 40, "italic"),
     text="Finnish")
+
+
 # Word text
-word_text = canvas.create_text(
-    400, 263,
-    fill="black",
-    font=("Ariel", 40, "bold"),
-    text="on")
+def set_word_text():
+    global canvas
+    random_word_entry = get_random_word(words_dict)
+    canvas.delete("word_text")
+    word_text = canvas.create_text(
+        400, 263,
+        fill="black",
+        font=("Ariel", 40, "bold"),
+        text=random_word_entry['finnish'].strip())
+
+
 incorrect_image = PhotoImage(file="images/wrong.png")
-incorrect_button = Button(image=incorrect_image, highlightthickness=0)
+incorrect_button = Button(image=incorrect_image, highlightthickness=0, command=set_word_text)
 incorrect_button.grid(row=1, column=0)
 correct_image = PhotoImage(file="images/right.png")
-correct_button = Button(image=correct_image, highlightthickness=0)
+correct_button = Button(image=correct_image, highlightthickness=0, command=set_word_text)
 correct_button.grid(row=1, column=1)
-
-words_dict = get_word_list(50)
-random_word_entry = get_random_word(words_dict)
-print(random_word_entry)
+set_word_text()
 
 window.mainloop()
