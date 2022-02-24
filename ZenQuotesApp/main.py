c133186e6c7da2bf773b9f471ace0cdb8486dc0a
Wayne Burris <waterseeker@Hotmail.com>
2022-02-24 13:23:33 -0600

    first pass of zen quotes app

--- ZenQuotesApp/main.py
+++ ZenQuotesApp/main.py
@@ -1,10 +1,14 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    response = requests.get("https://zenquotes.io/api/random")
+    response.raise_for_status()
+    data = response.json()[0]
+    quote = data["q"]
+    author = data["a"]
+    return f"{quote}\n\n{author}"
 
 
 window = Tk()
@@ -14,13 +18,11 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Zen Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text=get_quote(), width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 zen_img = PhotoImage(file="zen.png")
 kanye_button = Button(image=zen_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
-
 
 window.mainloop()
