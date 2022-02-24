5ebf10c26e386e8000eca4c7936f2397f9ba59cd
Wayne Burris <waterseeker@Hotmail.com>
2022-02-24 11:00:49 -0600

    added example of accessing data from a response and catching errors

--- ISSOverheadNotifier/making_api_calls.py
+++ ISSOverheadNotifier/making_api_calls.py
@@ -1,4 +1,20 @@
 import requests
 
 response = requests.get("http://api.open-notify.org/iss-now")
-print(response)
+# this only returns the response code
+
+# response codes generally follow the pattern:
+# 1XX: Hold On, processing, waiting for something
+# 2XX: Success, here you go
+# 3XX: Go Away
+# 4XX: You Screwed Up, resource doesn't exist
+# 5XX: I (the server) Screwed Up, internal error
+
+# you can raise exceptions for statuses by using
+response.raise_for_status()
+# you access the actual data from the response by using
+data = response.json()
+# you can manipulate this like any python dictionary
+# access a specific key
+latitude = data["iss_position"]["latitude"]
+longitude = data["iss_position"]["longitude"]
