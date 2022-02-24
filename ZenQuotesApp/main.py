d152e0218c8398fadf405b04e1cd2259f66afd06
Wayne Burris <waterseeker@Hotmail.com>
2022-02-24 13:27:49 -0600

    refactored get_quote to set text instead of returning a value

--- ZenQuotesApp/main.py
+++ ZenQuotesApp/main.py
@@ -8,7 +8,7 @@ def get_quote():
     data = response.json()[0]
     quote = data["q"]
     author = data["a"]
-    return f"{quote}\n\n{author}"
+    canvas.itemconfig(quote_text, text=f"{quote}\n\n{author}")
 
 
 window = Tk()
@@ -18,7 +18,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text=get_quote(), width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 zen_img = PhotoImage(file="zen.png")
