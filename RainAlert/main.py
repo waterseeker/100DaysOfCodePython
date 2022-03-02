00cee9e18eba1873069b9552d0499effb2f7d9a2
Wayne Burris <waterseeker@Hotmail.com>
2022-03-02 17:53:55 -0600

    working on making api call

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -1,5 +1,16 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
+MY_LATITUDE = os.getenv('MY_LATITUDE')
+MY_LONGITUDE = os.getenv('MY_LONGITUDE')
+parameters = {
+    "lat": MY_LATITUDE,
+    "lon": MY_LONGITUDE,
+    "exclude": ,
+    "appid": API_KEY
+}
+request = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LATITUDE}&lon={MY_LONGITUDE}&exclude={part}&appid={API_KEY}"
