525b5b1f277596e200137b8c0cd6404540f51d0a
Wayne Burris <waterseeker@Hotmail.com>
2022-03-05 18:57:43 -0600

    added getting a list of ingredients from a product bar code

--- FoodFacts/main.py
+++ FoodFacts/main.py
@@ -1,1 +1,8 @@
 import requests
+
+barcode = "049508001409"
+# barcode = input("Please enter a barcode to search for data for a product.")
+food_facts_url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
+food_ingredients_data = requests.get(food_facts_url).json()["product"]["ingredients"]
+ingredient_names = [ingredient["id"][3:] for ingredient in food_ingredients_data]
+print(ingredient_names)
