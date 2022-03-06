491d550dadf233fa2919404c9a3f4f5de7530670
Wayne Burris <waterseeker@Hotmail.com>
2022-03-06 17:26:54 -0600

    added flag so message only prints once

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -24,11 +24,13 @@ conditions = (day["weather"][0]["description"] for day in hourly_forecasts)
 counter = Counter(conditions)
 most_common_condition = counter.most_common(1)[0][0]
 # check if weather code is less than 700 in the next 12 hours, if so, alert the user they need to bring an umbrella
-next_12_hours = [hour["weather"][0]["id"] for hour in hourly_forecasts[:11]]
+next_12_hours = [hour["weather"][0]["id"] for hour in hourly_forecasts[:12]]
+will_rain = False
 for weather_id in next_12_hours:
-    if weather_id < 700:
-        print("You'll need to bring an umbrella")
-        continue
+    if int(weather_id) < 700:
+        will_rain = True
+if will_rain:
+    print("You'll need to bring an umbrella")
 print(f"Response status code: {response.status_code}")
 print(f"48 hour average temp will be: {average_temp_48_hour}")
 print(f"The most common weather condition for the next 48 hours will be: {most_common_condition}")
