faf3130629b88401d8a35a2ed2c02f84b1d0f31d
Wayne Burris <waterseeker@Hotmail.com>
2022-03-07 09:41:04 -0600

    added another product code

--- FoodFacts/main.py
+++ FoodFacts/main.py
@@ -1,6 +1,7 @@
 import requests
 
-barcode = "049508001409"
+barcode = "033919000458"
+# "049508001409"
 # barcode = input("Please enter a barcode to search for data for a product.")
 food_facts_url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
 food_ingredients_data = requests.get(food_facts_url).json()["product"]["ingredients"]
