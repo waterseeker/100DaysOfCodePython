f3daf3c92b3081408b3e005e337da733b7c85d06
Wayne Burris <waterseeker@Hotmail.com>
2022-03-14 13:32:41 -0500

    fixed username in env file, created a graph

--- HabitTracker/main.py
+++ HabitTracker/main.py
@@ -3,7 +3,33 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-TEST_ENV_VARIABLE = os.getenv('TEST_ENV_VARIABLE')
-print(TEST_ENV_VARIABLE)
+PIXELA_AUTH_TOKEN = os.getenv("PIXELA_AUTH_TOKEN")
+PIXELA_USERNAME = os.getenv("PIXELA_USERNAME")
+PIXELA_CREATE_USER_ENDPOINT = "https://pixe.la/v1/users"
 
-# requests.post()
+create_user_parameters = {
+    "token": PIXELA_AUTH_TOKEN,
+    "username": PIXELA_USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# # create an account
+# response = requests.post(url=PIXELA_CREATE_USER_ENDPOINT, json=create_user_parameters)
+# print(response.text)
+
+# create a graph
+graph_endpoint = f"{PIXELA_CREATE_USER_ENDPOINT}/{PIXELA_USERNAME}/graphs"
+graph_configuration = {
+    "id": "graph1",
+    "name": "Finnish Practice Graph",
+    "unit": "Days",
+    "type": "int",
+    "color": "shibafu",
+}
+headers = {
+    "X-USER-TOKEN": PIXELA_AUTH_TOKEN,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
+print(response.text)
