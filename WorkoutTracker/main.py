8f345cb81661b1caa896928b342e69f12f7c256d
Wayne Burris <waterseeker@Hotmail.com>
2022-03-15 10:41:32 -0500

    added post request and example of a response

--- WorkoutTracker/main.py
+++ WorkoutTracker/main.py
@@ -1,6 +1,39 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
-test = os.getenv("TEST")
-print(test)
+NUTRITIONIX_API_KEY = os.getenv("NUTRITIONIX_API_KEY")
+NUTRITIONIX_APP_ID = os.getenv("NUTRITIONIX_APP_ID")
+NUTRITIONIX_EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+USER_GENDER = os.getenv("USER_GENDER")
+USER_WEIGHT_KG = os.getenv("USER_WEIGHT_KG")
+USER_HEIGHT_CM = os.getenv("USER_HEIGHT_CM")
+USER_AGE = os.getenv("USER_AGE")
+
+# user_input = input("Please input what you did today:\n")
+user_input = "ran 3 miles"
+
+nutritionix_headers = {
+    "x-app-id": NUTRITIONIX_APP_ID,
+    "x-app-key": NUTRITIONIX_API_KEY,
+    "Content-Type": "application/json"
+}
+
+nutritionix_request_body = {
+    "query": user_input,
+    "gender": USER_GENDER,
+    "weight_kg": float(USER_WEIGHT_KG),
+    "height_cm": float(USER_HEIGHT_CM),
+    "age": int(USER_AGE)
+}
+
+response = requests.post(url=NUTRITIONIX_EXERCISE_ENDPOINT, json=nutritionix_request_body, headers=nutritionix_headers)
+response.raise_for_status()
+print(response.text)
+
+example_response = {"exercises": [
+    {"tag_id": 317, "user_input": "ran", "duration_min": 30.02, "met": 9.8, "nf_calories": 355.49,
+     "photo": {"highres": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_highres.jpg",
+               "thumb": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_thumb.jpg", "is_user_uploaded": false},
+     "compendium_code": 12050, "name": "running", "description": null, "benefits": null}]}
