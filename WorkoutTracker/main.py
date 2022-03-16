926f4ae9e1072331fc8751fa37dfd93ed8577e59
Wayne Burris <waterseeker@Hotmail.com>
2022-03-16 10:45:29 -0500

    added post request to sheety with formatted data

--- WorkoutTracker/main.py
+++ WorkoutTracker/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
+import datetime
 
 load_dotenv()
 NUTRITIONIX_API_KEY = os.getenv("NUTRITIONIX_API_KEY")
@@ -10,6 +11,7 @@ USER_GENDER = os.getenv("USER_GENDER")
 USER_WEIGHT_KG = os.getenv("USER_WEIGHT_KG")
 USER_HEIGHT_CM = os.getenv("USER_HEIGHT_CM")
 USER_AGE = os.getenv("USER_AGE")
+SHEETY_POST_ENDPOINT = os.getenv("SHEETY_POST_ENDPOINT")
 
 # user_input = input("Please input what you did today:\n")
 user_input = "ran 3 miles"
@@ -28,12 +30,28 @@ nutritionix_request_body = {
     "age": int(USER_AGE)
 }
 
-response = requests.post(url=NUTRITIONIX_EXERCISE_ENDPOINT, json=nutritionix_request_body, headers=nutritionix_headers)
-response.raise_for_status()
-print(response.text)
+nutritionix_response = requests.post(url=NUTRITIONIX_EXERCISE_ENDPOINT, json=nutritionix_request_body, headers=nutritionix_headers)
+nutritionix_response.raise_for_status()
+nutritionix_result = nutritionix_response.json()
 
-example_response = {"exercises": [
-    {"tag_id": 317, "user_input": "ran", "duration_min": 30.02, "met": 9.8, "nf_calories": 355.49,
-     "photo": {"highres": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_highres.jpg",
-               "thumb": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_thumb.jpg", "is_user_uploaded": false},
-     "compendium_code": 12050, "name": "running", "description": null, "benefits": null}]}
+# example_response = {"exercises": [
+#     {"tag_id": 317, "user_input": "ran", "duration_min": 30.02, "met": 9.8, "nf_calories": 355.49,
+#      "photo": {"highres": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_highres.jpg",
+#                "thumb": "https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_thumb.jpg", "is_user_uploaded": false},
+#      "compendium_code": 12050, "name": "running", "description": null, "benefits": null}]}
+
+now = datetime.datetime.now()
+current_date = now.strftime("%m/%d/%Y")
+current_time = now.strftime("%H:%M:%S")
+
+sheety_body = {
+    "workout": {
+        "date": current_date,
+        "time": current_time,
+        "exercise": nutritionix_result["exercises"][0]["name"].title(),
+        "duration": round(nutritionix_result["exercises"][0]["duration_min"]),
+        "calories": round(nutritionix_result["exercises"][0]["nf_calories"]),
+    }
+}
+
+sheety_response = requests.post(url=SHEETY_POST_ENDPOINT, json=sheety_body)
