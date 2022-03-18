b4b068d6f420b7c066a7621ed4cc18095e9d97a3
Wayne Burris <waterseeker@Hotmail.com>
2022-03-18 15:54:04 -0500

    added twilio client creation

--- FrugalFlightFinder/notification_manager.py
+++ FrugalFlightFinder/notification_manager.py
@@ -1,7 +1,10 @@
 # TODO create a client with .env variables
+# TODO create format for message
 # TODO create send message method
+
 import os
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 load_dotenv()
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
@@ -9,6 +12,7 @@ TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 RECEIVING_PHONE_NUMBER = os.getenv("RECEIVING_PHONE_NUMBER")
 
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
