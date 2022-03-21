8cfefbfa0601a2c8eda8c6ca1902bfcf44d085fe
Wayne Burris <waterseeker@Hotmail.com>
2022-03-21 10:43:54 -0500

    finished first pass for notification manager

--- FrugalFlightFinder/main.py
+++ FrugalFlightFinder/main.py
@@ -2,3 +2,11 @@ from data_manager import DataManager
 from flight_data import FlightData
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
+
+ticket_list = []
+
+data_manager = DataManager()
+flight_data = FlightData()
+flight_search = FlightSearch()
+if len(ticket_list) > 0:
+    notification_manager = NotificationManager(ticket_list)
--- FrugalFlightFinder/notification_manager.py
+++ FrugalFlightFinder/notification_manager.py
@@ -1,6 +1,3 @@
-# TODO create format for message
-# TODO create send message method
-
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
@@ -11,9 +8,22 @@ TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 RECEIVING_PHONE_NUMBER = os.getenv("RECEIVING_PHONE_NUMBER")
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
 
 class NotificationManager:
     # This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self, ticket_list):
+        self.ticket_list = ticket_list
+        self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    def send_message(self, ticket_list):
+        for ticket in ticket_list:
+            price = ticket['price']
+            origination = ticket['origination']
+            destination = ticket['destination']
+            travel_date = ticket['date']
+            message_body = f"Low price alert! Only ${price} to fly from {origination} to {destination}, from {travel_date}."
+            self.client.messages.create(
+                body=message_body,
+                from_=TWILIO_PHONE_NUMBER,
+                to=RECEIVING_PHONE_NUMBER
+            )
